d20421552534c6ecec271d79cb7eba58d645d554
Karl Liang <liang.karl@outlook.com>
2020-10-21 14:46:46 +0800

    Add installer for installing scripts

---
+++ lib/installer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+INSTALL_PACKAGES_LIST=(
+        'tmux'
+)
+
+install_packages()
+{
+        local len=${#INSTALL_PACKAGES_LIST[@]}
+        local package=
+
+        for((i = 0; i < len; i++)); do
+                package=${INSTALL_PACKAGES_LIST[$i]}
+                echo "Package: $package"
+                load_and_install_package $package
+        done
+}
+
+load_and_install_package()
+{
+        local script=${1}.sh
+        (
+                echo "Load script of package"
+                source $LIB_DIR/$script
+                install
+        )
+}
