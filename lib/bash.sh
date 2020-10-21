5fa157f85bf097c9d235d9a6ed16cdca02b123fb
Karl Liang <liang.karl@outlook.com>
2020-10-22 00:25:25 +0800

    Modify $PS1 when bash configuring

--- lib/bash.sh
+++ lib/bash.sh
@@ -23,4 +23,23 @@ config_package()
         local -r DST_CONFIG="$HOME/.bash_aliases"
         local -r SRC_CONFIG="$BASH_CONFIG/bash_aliases"
         create_link $SRC_CONFIG $DST_CONFIG
+
+        ## Modify $PS1
+        # Assume $PS1 are almost the same as default bashrc
+        local -r BASHRC="$HOME/.bashrc"
+        local -r BASHRC_TMP="${BASHRC}.tmp"
+        if [ -e $BASHRC ]; then
+                # Show current dir only, not full path.
+                sed 's:\\w\\:\\W\\:g' $BASHRC > $BASHRC_TMP
+
+                if ! cmp $BASHRC $BASHRC_TMP; then
+                        echo "Backup $BASHRC"
+                        mv $BASHRC ${BASHRC}.bak
+
+                        echo "Replace $BASHRC"
+                        mv $BASHRC_TMP $BASHRC
+                fi
+
+                rm $BASHRC_TMP
+        fi
 }
