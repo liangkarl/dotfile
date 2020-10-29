f39152b05e51b3cf7a47f31532751df59f51ea5b
Karl Liang <liang.karl@outlook.com>
2020-10-29 17:44:13 +0800

    Re-format bash.sh

--- lib/bash.sh
+++ lib/bash.sh
@@ -7,30 +7,30 @@ BASH_CONFIG="$CONFIG_DIR/$BASH_NAME"
 
 install()
 {
-        config_package
+	config_package
 }
 
 uninstall()
 {
-        echo "Remove $BASH_NAME..."
+	echo "Remove $BASH_NAME..."
 }
 
 config_package()
 {
-        echo "Config $BASH_NAME..."
+	echo "Config $BASH_NAME..."
 
-        ## Create config link
-        local -r DST_CONFIG="$HOME/.bash_aliases"
-        local -r SRC_CONFIG="$BASH_CONFIG/bash_aliases"
-        create_link $SRC_CONFIG $DST_CONFIG
+	## Create config link
+	local -r DST_CONFIG="$HOME/.bash_aliases"
+	local -r SRC_CONFIG="$BASH_CONFIG/bash_aliases"
+	create_link $SRC_CONFIG $DST_CONFIG
 
-        ## Modify $PS1
-        # Assume $PS1 are almost the same as default bashrc
-        local -r BASHRC="$HOME/.bashrc"
-        local -r BASHRC_TMP="${BASHRC}.tmp"
+	## Modify $PS1
+	# Assume $PS1 are almost the same as default bashrc
+	local -r BASHRC="$HOME/.bashrc"
+	local -r BASHRC_TMP="${BASHRC}.tmp"
 	if [ -e $BASHRC ]; then
 		cp $BASHRC $BASHRC_TMP
-                sed -i 's:\\w\\:\\W\\:g' $BASHRC_TMP
+		sed -i 's:\\w\\:\\W\\:g' $BASHRC_TMP
 		if ! cmp $BASHRC $BASHRC_TMP; then
 			echo "Replace and backup $BASHRC"
 			mv -vf $BASHRC $BASHRC.bak
@@ -40,5 +40,5 @@ config_package()
 		fi
 	else
 		echo "No $BASHRC found."
-        fi
+	fi
 }
