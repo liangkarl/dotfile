7274e29a7a480ed065bbc3247bc77b80f06dc30f
Karl Liang <liang.karl@outlook.com>
2020-10-29 16:22:03 +0800

    Link binaries under bin/

--- install.sh
+++ install.sh
@@ -7,6 +7,16 @@ source $ROOT/core/env.sh
 # prepare basic functions
 init_env $ROOT
 
+## Link bin/ to ~/bin/
+[[ ! -d $HOME/bin ]] && {
+	echo "Create 'bin' folder"
+	mkdir $HOME/bin
+}
+echo "Link files"
+# ln with soft link, verbose, interactive, backup
+ln -svib $ROOT/bin/* $HOME/bin/
+
+
 ## Install packages
 # Load package libraries
 source $CORE_DIR/installer.sh
