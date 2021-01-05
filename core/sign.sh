81afb2ac7291178e3b34b6febb38fd2ef69ad2ea
Karl Liang <liang.karl@outlook.com>
2021-01-05 15:28:13 +0800

    Fix sign func bugs

--- core/sign.sh
+++ core/sign.sh
@@ -7,15 +7,15 @@ add_with_sig()
     local -r FILE=$2
     local -r BY_WHO=${3:-sigh.sh}
     local -r DATE=$(date -I'date')
-    local -r USR_STAMP="# $USERNAME|$BY_WHO|$NOTE"
+    local -r USR_STAMP="# $USER|$BY_WHO|$NOTE"
     local -r TIME_STAMP="# $DATE created"
 
     # Default format:
-    # # $USERNAME|$BY_WHO|$NOTE
+    # # $USER|$BY_WHO|$NOTE
     # # $DATE created
     # added content here
     #
-    # Assume $USERNAME is from shell environment
-    local -r SIGNED_STAMP="\n${USR_STAMP}\n${TIME_STAMP}\n\n${CONTENT}\n"
+    # Assume $USER is from shell environment
+    local -r SIGNED_STAMP="\n${USR_STAMP}\n${TIME_STAMP}\n${CONTENT}\n"
     echo -e "$SIGNED_STAMP" >> $FILE
 }
