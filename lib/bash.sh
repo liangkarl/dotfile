325d17453811352fe85cbc87a320b2ac5839cff3
Karl Liang <liang.karl@outlook.com>
2021-01-05 13:52:48 +0800

    Divide bash_aliases and bug fixes

--- lib/bash.sh
+++ lib/bash.sh
@@ -19,32 +19,25 @@ config_package()
 {
 	echo "Config $BASH_NAME..."
 
-	## Create config link
-	local -r DST_CONFIG="$HOME/.bash_aliases"
-	local -r SRC_CONFIG="$BASH_CONFIG/bash_aliases"
-	create_link $SRC_CONFIG $DST_CONFIG
+    local -r ALIAS_FILES="
+        bash_aliases
+        alias_common
+        alias_working
+        init.bash
+    "
+    for FILE in $ALIAS_FILES; do
+        local DST="${HOME}/.${FILE}"
+        local SRC="${BASH_CONFIG}/${FILE}"
+        ## Create config link
+        create_link $SRC $DST
+    done
 
 	## Modify $PS1
 	# Assume $PS1 are almost the same as default bashrc
 	local -r BASHRC="$HOME/.bashrc"
-	local -r BASHRC_TMP="${BASHRC}.tmp"
-	if [ -e $BASHRC ]; then
-		cp $BASHRC $BASHRC_TMP
-		sed -i 's:\\w\\:\\W\\:g' $BASHRC_TMP
-		if ! cmp $BASHRC $BASHRC_TMP; then
-			echo "Replace and backup $BASHRC"
-			mv -vf $BASHRC $BASHRC.bak
-			mv $BASHRC_TMP $BASHRC
-		else
-			rm $BASHRC_TMP
-		fi
-	else
-		echo "No $BASHRC found."
-	fi
+    local -r INITRC="$HOME/.init.bash"
 
-	# Source customized init script
-	# local -r INIT_SCRIPT="$HOME/.init.bash"
-	# local -r SRC_SCRIPT="$BASH_CONFIG/init.bash"
-	# echo "[[ ! -e $INIT_SCRIPT ]] && . $SRC_SCRIPT" >> $BASHRC
-	# ln -s $INIT_SCRIPT $HOME/.init.bash
+    [ -f $BASHRC ] || cp /etc/skel/.bashrc $BASHRC
+
+    echo -e "\nif [ -f $BASHRC ]; then\n	. $INITRC\nfi\n\n" >> $BASHRC
 }
