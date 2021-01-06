81b4fef9c7b56cc8a0f3fdbea5360296cec09236
Karl Liang <liang.karl@outlook.com>
2021-01-06 15:57:59 +0800

    re-structure whole bash folder

--- lib/bash.sh
+++ lib/bash.sh
@@ -18,34 +18,50 @@ uninstall()
 
 config_package()
 {
-	echo "Config $BASH_NAME..."
+    echo "Config $BASH_NAME..."
 
-    local -r ALIAS_FILES="
-        bash_aliases
-        alias_common
-        alias_working
-        init.bash
-    "
-    for FILE in $ALIAS_FILES; do
-        local DST="${HOME}/.${FILE}"
-        local SRC="${BASH_CONFIG}/${FILE}"
-        ## Create config link
-        create_link $SRC $DST
-    done
-
-	## Modify $PS1
-	# Assume $PS1 are almost the same as default bashrc
-	local -r BASHRC="$HOME/.bashrc"
-    local -r INITRC="$HOME/.init.bash"
+    local -r USR_CONFIG="$HOME/.config/$BASH_NAME"
+    local -r BASHRC="$HOME/.bashrc"
     local -r BASH_COMPLETION="/etc/profile.d/bash_completion.sh"
+    local -r INITRC="init.bash"
+    local LOAD_CMD=''
+    local SRC=''
+
+    pushd $HOME
+    [ -e $USR_CONFIG ] || mkdir $USR_CONFIG
 
     [ -f $BASHRC ] || cp /etc/skel/.bashrc $BASHRC
+    SRC=$BASH_CONFIG
+    create_link $SRC/bash_aliases .bash_aliases
 
-    local LOAD_CMD=". $BASH_COMPLETION"
+    LOAD_CMD=". $BASH_COMPLETION"
     grep -wq "^$LOAD_CMD" $BASHRC ||\
         add_with_sig "$LOAD_CMD" "$BASHRC" "$BASH_NAME"
 
-    LOAD_CMD=". $INITRC"
+    LOAD_CMD=". $USR_CONFIG/$INITRC"
     grep -wq "^$LOAD_CMD" $BASHRC ||\
         add_with_sig "$LOAD_CMD" "$BASHRC" "$BASH_NAME"
+    popd
+
+    local -r ALIAS_DIR='alias'
+    local -r COMPLETION_DIR='completion'
+    pushd $USR_CONFIG
+    SRC=$BASH_CONFIG
+    create_link $SRC/init.bash .
+
+    [ -e $ALIAS_DIR ] || mkdir $ALIAS_DIR
+    pushd $ALIAS_DIR
+    SRC="$BASH_CONFIG/$ALIAS_DIR"
+    create_link $SRC/alias_common .
+    create_link $SRC/alias_working .
+    popd 
+
+    [ -e $COMPLETION_DIR ] || mkdir $COMPLETION_DIR
+    pushd $COMPLETION_DIR
+    SRC="$BASH_CONFIG/$COMPLETION_DIR"
+    #create_link $SRC/ .
+    popd
+    popd # $USR_CONFIG
+
+    sudo dpkg-reconfigure dash
 }
