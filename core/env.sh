d7b4cfcaa1dafb23f511d5b451f1e2c6704d2e7e
Karl Liang <karl.liang@quantatw.com>
2021-01-08 14:30:05 +0800

    Rename variable name

--- core/env.sh
+++ core/env.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-WHERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
-
 init_env()
 {
-        local -r ASSIGNED_PATH=$1
-        if [ -z "$1" ]; then
+	local WHERE="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pwd)"
+        local DEF_PATH="$1"
+        if [ -z "$DEF_PATH" ]; then
                 LIB_DIR="$WHERE/lib"
                 CONFIG_DIR="$WHERE/config"
                 CORE_DIR="$WHERE/core"
         else
-                LIB_DIR="$ASSIGNED_PATH/lib"
-                CONFIG_DIR="$ASSIGNED_PATH/config"
-                CORE_DIR="$ASSIGNED_PATH/core"
+                LIB_DIR="$DEF_PATH/lib"
+                CONFIG_DIR="$DEF_PATH/config"
+                CORE_DIR="$DEF_PATH/core"
         fi
 }
