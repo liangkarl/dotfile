8b7e65e3d25c77e731a920cab5836741c2bad1ac
Karl Liang <karl.liang@quantatw.com>
2021-01-08 13:40:19 +0800

    Change to setup_version mainly

--- lib/others.sh
+++ lib/others.sh
@@ -54,8 +54,11 @@ install_llvm_clang()
 
 	sudo apt install libclang-10-dev
 	popd # leave .tmp
-	rm -r .tmp
+	rm -rf .tmp
 	popd # leave $HOME
+
+	# FIXME: only support clang-X
+	setup_version "/usr/bin/clang" "clang" "$(ls /usr/bin/clang-[0-9])"
 }
 
 install_ccls()
@@ -64,16 +67,15 @@ install_ccls()
 
 	echo "Install ccls with snap."
 	# Reference https://github.com/MaskRay/ccls/issues/609
-	sudo apt install -y snapd
+	# sudo apt install -y snapd
+
 	# Check status
 	# systemctl status snapd.service
 	# Run snapd, if not yet active
 	# systemctl start snapd.service
 	# install ccls by snap
-	if test_cmd 'snap'; then
-		sudo snap install ccls --classic \
+	sudo snap install ccls --classic \
 			&& return
-	fi
 
 	echo "Try to build ccls"
 	install_llvm_clang
@@ -87,11 +89,17 @@ install_ccls()
 
 	git clone --depth=1 --recursive https://github.com/MaskRay/ccls
 	cd ccls
+	echo "*** Make sure you have workable version for ccls ***"
+	echo "g++: $(g++ --version)" | awk 'NR==1'
+	echo "gcc: $(gcc --version)" | awk 'NR==1'
+	echo "clang: $(clang --version)" | awk 'NR==1'
+	echo "cmake: $(cmake --version)" | awk 'NR==1'
+	echo "****************************************************"
 	cmake -H. -BRelease \
 		-DCMAKE_BUILD_TYPE=Release \
 		-DCMAKE_PREFIX_PATH=/usr/lib/llvm-10 \
 		-DLLVM_INCLUDE_DIR=/usr/lib/llvm-10/include \
-		-DLLVM_BUILD_INCLUDE_DIR=/usr/include/llvm-10/
+		-DLLVM_BUILD_INCLUDE_DIR=/usr/include/llvm-10
 	cmake --build Release
 	sudo make install -C ./Release
 
@@ -104,7 +112,21 @@ install_gcc()
 {
 	sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 	sudo apt update
-	sudo apt install gcc-9 gcc-8
+	sudo apt install gcc-7 gcc-8 gcc-9
+	setup_version "/usr/bin/gcc" "gcc" "$(ls /usr/bin/gcc-[0-9])"
+
+	sudo apt install g++-7 g++-8 g++-9
+	setup_version "/usr/bin/g++" "g++" "$(ls /usr/bin/g++-[0-9])"
+}
+
+install_python()
+{
+	sudo add-apt-repository ppa:deadsnakes/ppa
+	sudo apt update
+	sudo apt install python3.6 python3.9
+	# FIXME: only matched regex: pythonX.Y
+	setup_version "/usr/bin/python" "python" "$(ls /usr/bin/python[0-9].[0-9])"
+	setup_version "/usr/bin/python3" "python3" "$(ls /usr/bin/python3.[0-9])"
 }
 
 uninstall()
