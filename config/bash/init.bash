74b121e8d010b4459542e55c9fd4a28e8b4249ed
Karl Liang <karl.liang@quantatw.com>
2021-01-12 11:22:02 +0800

    Gen compile_commands.json while compile Android

--- config/bash/init.bash
+++ config/bash/init.bash
@@ -1,6 +1,13 @@
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
+# Used for Android
+## Generate compile_commands.json
+export SOONG_GEN_COMPDB=1
+export SOONG_GEN_COMPDB_DEBUG=1
+# Make soong generate a symlink to the compdb file using an env var
+# export SOONG_LINK_COMPDB_TO=$ANDROID_HOST_OUT
+
 PS1='\[\033[01;38m\][\t] ' # Current time
 PS1+='${debian_chroot:+($debian_chroot)}'
 PS1+='\[\033[01;32m\]\u@\h'
