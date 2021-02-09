ce37ff78e076a21aa28fd4600f39714b15fb9bc7
Karl Liang <liang.karl@outlook.com>
2021-02-09 13:57:44 +0800

    Fix many problems in adb

--- tools/adb_helper.sh
+++ tools/adb_helper.sh
@@ -3,42 +3,60 @@
 __ADB=$(which adb)
 __ADB=${__ADB:-./adb.exe}
 
+__adb_animate_star() {
+	local COUNT IMAGE
+	COUNT="$1"
+	IMAGE=$((COUNT % 4))
+	case $IMAGE in
+		1) echo -en '\';;
+		2) echo -en '|';;
+		3) echo -en '/';;
+		0 | *) echo -en '-'
+	esac
+}
+
+__adb_animate_cmd() {
+	local char
+	chars="/-\|"
+
+	while :; do
+		for (( i=0; i<${#chars}; i++ )); do
+			sleep 0.5
+			echo -en "${chars:$i:1}" "\r"
+		done
+	done
+}
+
 adb_shell() {
-	local CMD
-	CMD="$@"
-	echo "cmd: $CMD"
-	$__ADB shell $CMD
+	# echo "cmd: $CMD"
+	$__ADB shell "$@"
 	echo "adb_shell: ok"
 }
 
 adb_root() {
-	local COUNT CMD INTV
+	local COUNT INTV BREAK STR
 	COUNT=0
-	INTV=0.1
-	while [ true ]; do
-		[ $COUNT -eq 10 ] && echo "disable error output."
-
-		if [ $COUNT -lt 10 ]; then
-			$__ADB root
-		else
-			$__ADB root >&-
-		fi
+	INTV=0.3
+	BREAK='false'
+	while [ $BREAK == 'false' ]; do
+		$__ADB root &> /dev/null && {
+			echo " adb_root: ok"
+			break;
+		}
+		echo -en "\rkeep trying to get root permission... $(__adb_animate_star $COUNT)"
 		COUNT=$((COUNT + 1))
 		sleep $INTV
 	done
-	echo "adb_root: ok"
 }
 
 adb_loop_shell_cmd() {
-	local INTV CMD
-	CMD="$@"
+	local INTV
 	INTV=0.1
 	echo "sleep: $SLEEP sec"
-	echo "cmd: $CMD"
-	echo ""
 	while [ true ]; do
 		sleep $INTV
-		$__ADB shell $CMD
+		# <C-c> to stop
+		adb_shell "$@"
 	done
 }
 
@@ -51,9 +69,9 @@ adb_kmsg_grepper() {
 }
 
 adb_ints_grepper() {
-	local INT KEYWORD
+	local INT KEYWORD LOOP_READ
 	KEYWORD="$@"
 	INT='/proc/interrupts'
-	adb_root
-	adb_shell "head -1 $INT; while 0; do cat $INT | grep $KEYWORD; done"
+	LOOP_READ="while :; do cat $INT | grep $KEYWORD | tr '\n' '\r'; done"
+	adb_shell "head -1 $INT; $LOOP_READ"
 }
