1659b0ab01a31cf505e5f5370ca46c387e5e1e08
Karl Liang <karl.liang@quantatw.com>
2021-02-18 15:43:29 +0800

    Add more gdb related tools

--- lib/gdb.sh
+++ lib/gdb.sh
@@ -5,20 +5,25 @@ source $SHELL_CORE_DIR/core.sh
 GDB='gdb'
 GDB_DIR="$SHELL_CONFIG_DIR/$GDB"
 
-gdb_install_guigdb()
+gdb_install_gdb()
 {
-	local NEED_PACK
+    sudo apt install -y $GDB
+}
 
-    NEED_PACK='wget pip'
-	has_cmd $NEED_PACK || {
-        show_err "$(info_req_cmd $NEED_PACK)"
+gdb_install_gdbgui()
+{
+    local NEED_CMD
+
+    NEED_CMD=python3
+
+	has_cmd $NEED_CMD || {
+        show_err "$(info_req_cmd $NEED_CMD)"
 		return $BAD
 	}
 
-    goto $HOME
-    wget -P ~ https://git.io/.gdbinit
-    pip install pygments
-    back
+    python3 -m pip install --user pipx
+    python3 -m userpath append $HOME_BIN_DIR
+    pipx install gdbgui
 }
 
 gdb_install()
@@ -48,6 +53,36 @@ gdb_remove()
 
 gdb_config()
 {
+    local FORCE ALL
+    FORCE=$(echo $1 | grep force)
+    ALL=$(declare -F | awk '{print $3}' | grep -E "^${GIT}_config_")
+
+    [ -z $FORCE ] && {
+        has_cmd $GIT || break
+
+		show_hint "$(info_installed $GIT)"
+		return
+	}
+
+    echo "Config $GIT..."
+    for EXEC in $ALL; do
+        $EXEC
+    done
+}
+
+gdb_config_gdbinit()
+{
+	local NEED_CMD
+
+    NEED_CMD='wget pip'
+	has_cmd $NEED_CMD || {
+        show_err "$(info_req_cmd $NEED_CMD)"
+		return $BAD
+	}
+
 	echo "Config $GDB..."
-	echo "Not finished yet..."
+    goto $HOME
+    wget -P ~ https://git.io/.gdbinit
+    pip install pygments
+    back
 }
