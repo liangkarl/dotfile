79bf08f4911a757bee306a1da7a6aea58bea200b
Karl Liang <karl.liang@quantatw.com>
2021-03-18 15:46:38 +0800

    Add knock_helper for script

--- config/bash/init.bash
+++ config/bash/init.bash
@@ -8,6 +8,9 @@ export SOONG_GEN_COMPDB_DEBUG=1
 # Make soong generate a symlink to the compdb file using an env var
 # export SOONG_LINK_COMPDB_TO=$ANDROID_HOST_OUT
 
+# FIXME: hard code here
+export SHELL_DIR=$HOME/shell
+
 __customize_ps1() {
     local WHITE='\[\033[01;38m\]'
     local BLUE='\[\033[01;34m\]'
@@ -28,6 +31,45 @@ __customize_ps1() {
     PS1+=${LYELLOW}'\$'${RESET}' '
 
     export PS1
+}
+
+__knock_helper() {
+    local NAME FILE
+    local LIST DONE
+    local POSTFIX='_helper.sh'
+
+    LIST=($(ls ${SHELL_DIR}/tools/*${POSTFIX}))
+    if [ -z "$LIST" ]; then
+        return 128
+    fi
+
+    for ((i = 0; i < $#; i++)); do
+        DONE=n
+        for ((j = 0; j < ${#LIST[@]}; j++)); do
+            FILE=$(basename ${LIST[$j]})
+            NAME=${FILE//${POSTFIX}/}
+            if [[ "$1" == $NAME ]]; then
+                echo "call helper: $NAME"
+                . ${LIST[$j]}
+                DONE=y
+                break
+            fi
+        done
+
+        if [[ $DONE != y ]]; then
+            echo "unsupport helper: $1"
+            return 128
+        fi
+        shift
+    done
+}
+
+__knock_helper_compl() {
+    local LIST POSTFIX
+    POSTFIX='_helper.sh'
+    LIST="$(cd ${SHELL_DIR}/tools/ && { ls *${POSTFIX} | sed "s/$POSTFIX//g"; })"
+
+    COMPREPLY=($(compgen -W "$LIST" "${COMP_WORDS[1]}"))
 }
 
 __customize_ps1
