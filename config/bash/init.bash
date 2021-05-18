7c2e00d51876faebcd7ba3f76f3e9acbd5226fc3
Karl Liang <karl.liang@quantatw.com>
2021-05-18 09:36:30 +0800

    Optimize PS1 string performance
    1. Remove `tput sgr0` since performance problem
    2. Change RESET to ANSI code to reduce executed time
    3. Remove unecessary back slach

--- config/bash/init.bash
+++ config/bash/init.bash
@@ -12,18 +12,18 @@ export SOONG_GEN_COMPDB_DEBUG=1
 export SHELL_DIR=$HOME/shell
 
 __customize_ps1() {
-    local WHITE='\[\033[01;38m\]'
-    local BLUE='\[\033[01;34m\]'
-    local LYELLOW='\[\033[38;5;11m\]'
-    local GREEN='\[\033[01;32m\]'
-    local PURPLE='\[\033[38;5;63m\]'
-    local SED_PURPLE='\\\[\\\033[38;5;63m\\\]'
-    local SED_ORANGE='\[\\\033[38;5;202m\\\]'
-    local RESET='\[$(tput sgr0)\]'
+    local WHITE='\033[01;38m'
+    local BLUE='\033[01;34m'
+    local LYELLOW='\033[38;5;11m'
+    local GREEN='\033[01;32m'
+    local PURPLE='\033[38;5;63m'
+    local SED_PURPLE='\\\033[38;5;63m'
+    local SED_ORANGE='\\\033[38;5;202m'
+    local RESET='\033[0m'
 
     PS1=${WHITE}'[\t] ' # Current time
     PS1+='${debian_chroot:+($debian_chroot)}'
-    PS1+="${PURPLE}\$? "
+    PS1+=${PURPLE}'$? '
     PS1+=${GREEN}'\u@\h '
     PS1+=${BLUE}'\w '
     PS1+='$(git branch 2>&- | sed -e "/^[^*]/d" -e "s/* \(.*\)/'${SED_PURPLE}'-> '${SED_ORANGE}'\1/")'
