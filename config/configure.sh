7cae04c4141b75bc33663b8283cb2a837c2b2ea4
Karl Liang <karl.liang@quantatw.com>
2021-07-15 21:44:38 +0800

    Fix cannot find '~/.config' problem

--- config/configure.sh
+++ config/configure.sh
@@ -3,8 +3,12 @@
 # TODO: need more flexible setup
 
 # Copy config to right place
-target="${XDG_CONFIG_HOME:-~/.config}"
+target="${XDG_CONFIG_HOME:-${HOME}/.config}"
 config="$(dirname $0)"
+
+if [[ ! -e "$target" ]]; then
+    mkdir $target
+fi
 
 # FIXME: don't copy this script
 cp -vr ${config}/* $target/
