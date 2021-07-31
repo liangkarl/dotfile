b0da72d9f9fa60315d393309e2ff0c1552f3010b
Karl Liang <liang.karl@outlook.com>
2021-07-31 15:23:05 +0800

    Rewrite configure.sh due to different dir layout

--- tools/configure.sh
+++ tools/configure.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-target="$HOME"
+target="${XDG_CONFIG_HOME:-${HOME}/.config/shell}"
 self="$(basename $0)"
 tools_dir="$(dirname $0)"
 
-if [[ ! -e "$target" ]]; then
+if [[ ! -d "$target" ]]; then
     mkdir $target
 fi
+cp -r $tools_dir $target
 
-echo "Copy bin/ to $target"
-cp -r ${tools_dir}/bin ${target}/
+bin_dir="${HOME}/bin"
+if [[ ! -d "$bin_dir" ]]; then
+    mkdir $bin_dir
+fi
+echo "link $target to ~/bin"
+
+list="$(find ${tools_dir} -type f -exec 'readlink' '-e' '{}' ';')"
+for file in ${list[@]}; do
+    ln -s $file $bin_dir
+done
+
+# clean self
+rm -f ${target}/$(basename ${tools_dir})/${self}
+rm -f ${HOME}/bin/${self}
