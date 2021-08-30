5c49a433188baf60684f9af5d95fdc320e9eba60
Karl Liang <karl.liang@quantatw.com>
2021-08-30 03:24:32 +0000

    tmux: update install script

--- install/staging/tmux.sh
+++ install/staging/tmux.sh
@@ -1,70 +1,66 @@
 #!/bin/bash
 # tmux installer script
 
-# source $SHELL_CORE_DIR/core.sh
+usage() {
+    echo "$(basename $0) [apt | github]"
+}
 
-tmux_install() {
+github_install() {
+    # URL: https://github.com/tmux/tmux
     local url='https://github.com/tmux/tmux.git'
     local pos="/tmp/install/tmux"
 
-    if [[ ! -z "$(type -p tmux)" ]]; then
-        echo "you have install tmux already"
+    # NOTE: need packages
+    # autoconf, automake, pkconf, libevent-dev
+    sudo apt install autoconf automake pkg-config libevent-dev
+    sudo apt autoremove
+
+    # remove install dir
+    trap "rm -rf $pos" EXIT ERR
+
+    # download git
+    git clone --depth=1 $url $pos
+
+    # build tmux
+    cd $pos
+    sh autogen.sh
+    ./configure
+    make
+    sudo make install
+    cd -
+}
+
+apt_install() {
+    sudo apt install $bin
+}
+
+trap 'pr_err "operation failed ($?)"' ERR
+set -eE
+
+bin='tmux'
+if [[ ! -z "$(type -p $bin)" ]]; then
+    pr_warn "you have install $bin already"
+fi
+
+if [[ ! -z "$1" ]]; then
+    if [[ -z "$(type ${1}_install)" ]]; then
+        usage
         exit 2
     fi
 
-    PS3='select install source > '
-    select option in 'github' 'apt' 'cancel'; do
-        case $option in
-            github) # https://github.com/tmux/tmux
-
-                # remove install dir
-                trap 'rm -rf $pos' EXIT
-
-                # download git
-                git clone --depth=1 $url $pos
-
-                # build tmux
-                cd $pos
-                sh autogen.sh
-                ./configure
-                make
-                sudo make install
-                cd -
-
-                ;;
-            apt)
-                sudo apt install tmux
-                ;;
-            cancel)
-                exit 0
-                ;;
-        esac
-    done
-
-    echo "install complete"
-}
-
-usage() {
-    echo "$(basename $0) [install]"
-}
-
-trap 'echo "operation failed ($?)"' ERR
-set -eE
-
-if (( $# == 0 )); then
-    usage
-    exit 1
+    eval ${1}_install
+    exit 0
 fi
 
-while (( $# != 0 )); do
-    case $1 in
-        install)
-            tmux_install
-            ;;
-        remove)
-            tmux_remove
-            ;;
+PS3='select install source > '
+select src in 'github' 'apt' 'cancel'; do
+    case $src in
+        cancel)
+            exit 0;;
         *)
-            break
+            eval ${src}_install
     esac
+    break
 done
+
+pr_good "install complete: $bin"
