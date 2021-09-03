355827c19635f1248fec46fd2c0ce693974dc60c
Karl Liang <karl.liang@quantatw.com>
2021-09-03 02:13:46 +0000

    config: update tools config
    1. enable extglob to avoid copy config script or some black list files
    2. copy all dir under tools into shell/ instead of scattering in .config/
    3. add some notes and echo more detailed info
    4. exit script once error occurred

--- tools/configure.sh
+++ tools/configure.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-target="${XDG_CONFIG_HOME:-${HOME}/.config/shell}"
+set -eE
+shopt -s extglob
+
+# copy tool dirs to user config dir
 self="$(basename $0)"
 tools_dir="$(dirname $0)"
-
-if [[ ! -d "$target" ]]; then
-    mkdir $target
+config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}/shell"
+black_list="$self|test"
+if [[ ! -d "$config_dir" ]]; then
+    mkdir $config_dir
 fi
-cp -r $tools_dir $target
+echo "-- Copy tools to $config_dir --"
+cp -r $tools_dir/!(${black_list}) $config_dir
 
+# link executable files to $HOME/bin
 bin_dir="${HOME}/bin"
 if [[ ! -d "$bin_dir" ]]; then
     mkdir $bin_dir
 fi
-echo "link $target to ~/bin"
-
-list="$(find ${tools_dir} -type f -exec 'readlink' '-e' '{}' ';')"
+echo "-- Link $config_dir to $bin_dir --"
+list="$(find ${config_dir} -type f -exec 'readlink' '-e' '{}' ';')"
 for file in ${list[@]}; do
-    ln -s $file $bin_dir
+    ln -sf $file $bin_dir
 done
-
-# clean self
-rm -f ${target}/$(basename ${tools_dir})/${self}
-rm -f ${HOME}/bin/${self}
