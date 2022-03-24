32ff9a97251d117c8f4e4ad441adff55dcb3b915
Karl Liang <liang.karl@outlook.com>
2022-03-24 23:38:06 +0800

    tool: simplify gen-patch script

--- tools/bin/gen-patch.sh
+++ tools/bin/gen-patch.sh
@@ -1,98 +1,93 @@
 #!/bin/bash
 
-error() {
-    echo $@ >&2
+usage() {
+    local n
+
+    n=$(basename $0)
+    cat <<-EOF
+    $n -s start
+    $n -e end
+    $n -a patch.zip
+EOF
+
+    exit $1
 }
 
-verify() {
-    local patch_dir zip origin
-    origin=$1
-    patch_dir=$2
-    zip=$3
+apply() {
+    local tmp_cmd sha_list
 
-    echo "clean $patch_dir in all projects"
-    repo forall -c 'rm -rf '$patch_dir''
-    tar -xvf $zip
-    repo forall -pvc 'git am -sk --ignore-whitespace '${patch_dir}'/* 2>&-'
+    tmp_cmd=$(mktemp)
+    sha_list=$(mktemp)
 
-    local ans cmd
-
-    read -p "everything fine? [Y/n] " ans
-    if [[ "$ans" == 'n' ]]; then
-        cmd='git am --abort 2>&-;'
-        read -p "back to previous patch? [Y/n] " ans
-        if [[ "$ans" != 'n' ]];  then
-            repo forall -c "$cmd"
-            echo "complete"
-            return 0
-        fi
+    cat > $tmp_cmd <<-EOF
+    rev="$(git rev-parse HEAD)"
+    echo "${rev},$(pwd)" >> $sha_list
+    git am -sk --ignore-whitespace '${PATCH_OUT}'/*
+    if (( $? != 0 )); then
+        echo "recover working tree due to 'am' error. ($?)"
+        git am --abort
+        git reset --hard $rev
     fi
+    EOF
+    tar -xvf $1
+    repo forall -pvc "bash $tmp_cmd"
 
     read -p "reset/clean to original HEAD? [Y/n] " ans
-    if [[ "$ans" != 'n' ]]; then
-        cmd+="git reset --hard $origin; rm -r $patch_dir"
-    fi
-    repo forall -c "$cmd"
-    return 0
+    [[ "${ans,,}" == n ]] && exit
+
+    cat > $tmp_cmd <<-EOF
+    rev="$(cat $sha_list | awk -F, '/'$(pwd)'/{print $1}')"
+    echo "$(pwd),${rev}" >> $sha_list
+    git reset --hard $rev
+    rm -rf $PATCH_OUT
+    EOF
+    repo forall -pvc "bash $tmp_cmd"
+
+    exit
 }
 
-gen_patch() {
-    local git repo tar
-    git=$(which git)
-    repo=$(which repo)
-    tar=$(which tar)
+PATCH_OUT='.patch'
 
-    if (( $# == 0 )); then
-        error "no valid input variables"
-        declare -f $FUNCNAME
-        return 2
-    fi
+(( $# == 0 )) && usage 1
 
-    if [[ "$git" == "" ]] || [[ "$repo" == "" ]] || [[ "$tar" == "" ]]; then
-        error "make sure that you've installed git, repo or tar"
-        return 1
-    fi
+while (( $# != 0 )); do
+    case $1 in
+        -s) shift
+            from="$1"
+            ;;
+        -e) shift
+            target="$1"
+            ;;
+        -a) shift
+            afile="$1"
+            ;;
+        *)
+            usage 2
+            ;;
+    esac
+    shift
+done
 
-    local from target rev_range
+# exit script if verify enabled
+[[ ! -z "$afile" ]] && apply $afile
 
-    if (( $# == 2 )); then
-        from="$1"
-        target="$2"
-    elif (( $# == 1 )); then
-        from='__begin'
-        target="$1"
-    fi
+[[ -z "$from" ]] && usage 3
+[[ -z "$target" ]] && target='HEAD'
 
-    local cmd
+# TODO: check ${from} is available for this git or not
+tmp_cmd=$(mktemp)
+cat > $tmp_cmd <<-EOF
+git format-patch --binary -k -o $PATCH_OUT ${from}..${target}
+[[ -z "$(ls -A $PATCH_OUT)" ]] && rm -rf $PATCH_OUT
+EOF
 
-    echo "generate patch from $from to $target ..."
-    if [[ "$from" == '__begin' ]]; then
-        echo "set tag to HEAD for temp"
-        cmd="git tag ${from}"
-    fi
-    cmd+="git format-patch --binary -k -o $target ${from}..${target};"
-    cmd+="rmdir $target 2>&-"
-    repo forall -pvc "$cmd"
+echo "generate patch from $from to $target ..."
+repo forall -pvc "bash $tmp_cmd"
 
-    local name
+name=${target}_patchset.bz2
+echo "compress patch files, $name ..."
+repo list | awk '{print $1"/'$PATCH_OUT'"}'| xargs tar -cjf $name 2>&-
+repo manifest -r -o "${from,,}.xml"
 
-    name=${target}_patchset.bz2
-    echo "compress patch files, $name ..."
-    repo list | awk '{print $1"/'${target}'"}'| xargs tar -cjf $name 2>&-
-
-    if [[ "$from" == '__begin' ]]; then
-        echo "remove tmp tag"
-        repo forall -c "git tag -d ${from} 2>&-"
-    fi
-
-    name=${from,,}.xml
-    repo manifest -r -o "$name"
-
-    echo "complete"
-
-    verify $from $target ${target}_patchset.bz2
-
-    return 0
-}
-
-gen_patch $@
+rm -rf $tmp_cmd
+echo "complete"
