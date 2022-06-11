7772f29cbbfbf2aa864c70ba24117f040c0099d9
Karl Liang <liang.karl@outlook.com>
2022-06-11 23:25:58 +0800

    bash: add extra comment for fzf option

--- config/bash/init/fzf.sh
+++ config/bash/init/fzf.sh
@@ -4,3 +4,6 @@ export FZF_DEFAULT_COMMAND='fd --type f'
 
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# Default options
+# export FZF_DEFAULT_OPTS="--layout=reverse --inline-info"
