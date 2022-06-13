18c45f6d0cb5a11ca91b3081f77bd2b5123ea137
Karl Liang <liang.karl@outlook.com>
2022-06-13 09:47:49 +0800

    bash: add exported variables for 'enhancd'

---
+++ config/bash/init/enhancd.sh
@@ -0,0 +1,7 @@
+# https://github.com/b4b4r07/enhancd
+# The ENHANCD_DIR variable is a base directory path. It defaults to ~/.enhancd.
+export ENHANCD_DIR="$XDG_CONFIG_HOME/enhancd"
+# disable interactive menu of `cd`
+export ENHANCD_DISABLE_HOME=1
+# don't override `cd ..` by default
+export ENHANCD_DOT_ARG='...'
