8cff908772ce12ec6cf0bf621474507e1387512a
Karl Liang <liang.karl@outlook.com>
2022-08-16 09:58:21 +0800

    nvim: add lua LSP server support

--- config/nvim/lua/config/nvim-lspconfig.lua
+++ config/nvim/lua/config/nvim-lspconfig.lua
@@ -54,4 +54,29 @@ lsp.ccls.setup {
   }
 }
 
+-- Lua (sumneko_lua)
+-- https://github.com/sumneko/lua-language-server
+lsp.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 vim.keymap.set('n', '<Leader>li', ':LspInfo<CR>')
