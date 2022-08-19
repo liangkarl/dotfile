6ee1a4ce8e9aab5dfd49c1c61ea2a843502ab1f2
Karl Liang <liang.karl@outlook.com>
2022-08-19 10:59:57 +0800

    nvim: add jump-to-somewhere keybinds

--- config/nvim/lua/config/nvim-lspconfig.lua
+++ config/nvim/lua/config/nvim-lspconfig.lua
@@ -25,9 +25,34 @@
 local lsp = require('lspconfig')
 local aerial = require('aerial')
 
--- required by aerial.nvim to support LSP function
+local cust_attach = function (client, bufnr)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  -- If the buffer did not supported by LSP, these keymap would not take
+  -- affect.
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', '<space>gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', '<space>gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', '<space>gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<space>gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>sh', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<space>s?', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', '<space>lf', vim.lsp.buf.formatting, bufopts)
+
+  -- required by aerial.nvim to support LSP function
+  aerial.on_attach(client, bufnr)
+end
+
 lsp.vimls.setup {
-  on_attach = aerial.on_attach,
+  on_attach = cust_attach,
 }
 
 -- Bash (bash-language-server)
@@ -58,7 +83,7 @@ lsp.ccls.setup {
     };
   },
 
-  on_attach = aerial.on_attach,
+  on_attach = cust_attach,
 }
 
 -- Lua (sumneko_lua)
@@ -85,7 +110,13 @@ lsp.sumneko_lua.setup {
     },
   },
 
-  on_attach = aerial.on_attach,
+  on_attach = cust_attach,
 }
 
 vim.keymap.set('n', '<Leader>li', ':LspInfo<CR>')
+
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>ge', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '<space>gp', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', '<space>gn', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>gq', vim.diagnostic.setloclist, opts)
