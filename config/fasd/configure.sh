932472e75e1aa609a06101a414e9a37f50663dd2
Karl Liang <liang.karl@outlook.com>
2023-04-05 17:29:17 +0800

    fasd: add install script for fasd

---
+++ config/fasd/configure.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -x
+
+dir="$(realpath -m -s $(dirname $0))"
+dir=${1:-$dir}
+tmp="$(mktemp -d)"
+line='eval "$(fasd --init auto)"'
+
+trap "rm -rf $tmp" EXIT
+
+git clone -n --depth=1 https://github.com/clvv/fasd.git $tmp
+
+cp -rf ${tmp}/.git $dir
+git -C $dir reset --hard HEAD
+
+ln -sf $dir/fasd ~/bin/fasd
+ln -sf $dir/fasdrc ~/.fasdrc
+grep -q -w "$line" ~/.bashrc ||
+        echo -e "\n$line" >> ~/.bashrc
