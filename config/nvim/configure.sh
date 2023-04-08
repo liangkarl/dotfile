ceeea13eaff6aba4f92aa4a546068765188ae052
Karl Liang <liang.karl@outlook.com>
2023-04-08 17:40:07 +0800

    nvim: install plugin after copy nvim config

--- config/nvim/configure.sh
+++ config/nvim/configure.sh
@@ -7,3 +7,5 @@ echo "-- setup nvim config --"
 # link user-level config of EditorConfig
 ln -sf ${config_dir}/nvim/editorconfig ~/.editorconfig
 ln -sf ${config_dir}/nvim/clang-format ~/.clang-format
+
+nvim +"PlugInstall" <<< "Close the window after all of the plugins installed."
