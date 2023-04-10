2ae82d9ed2675d276c5678011c9b239308fca528
karl.liang <karl.liang@quantatw.com>
2023-04-10 15:01:50 +0800

    enhancd: add some parameters for fzf and skim

--- config/enhancd/bootstrap.sh
+++ config/enhancd/bootstrap.sh
@@ -5,5 +5,10 @@
 export ENHANCD_DIR="$XDG_CONFIG_HOME/enhancd"
 # don't override `cd ..` by default
 export ENHANCD_ARG_DOUBLE_DOT='...'
+
+__enhancd_fzf_filter="fzf --preview 'ls -v --group-directories-first -N --color=auto -l --time-style=long-iso {}' --preview-window right,50% --height 35% --reverse --ansi"
+
+# there is a TUI issue for preview height in skim.
+__enhancd_skim_filter="sk --preview 'ls -v --group-directories-first -N --color=auto -l --time-style=long-iso {}' --preview-window right,50%  --reverse --ansi"
 # config enhanced
-export ENHANCD_FILTER="fzf --preview 'ls -v --group-directories-first -N --color=auto -l --time-style=long-iso {}' --preview-window right,50% --height 35% --reverse --ansi"
+export ENHANCD_FILTER="$__enhancd_fzf_filter:$__enhancd_skim_filter"
