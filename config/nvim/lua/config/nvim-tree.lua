5b8cecab7321b668e3f4b4103cea5422cc4d2739
Karl Liang <liang.karl@outlook.com>
2023-04-12 21:57:03 +0800

    nvim: remove unnecessary keybind and .vim config

--- config/nvim/lua/config/nvim-tree.lua
+++ config/nvim/lua/config/nvim-tree.lua
@@ -1,5 +1,17 @@
 -- Plugin: nvim-tree
 -- https://github.com/kyazdani42/nvim-tree.lua
+-- |------+------------------------------|
+-- | key  | explain                      |
+-- |------+------------------------------|
+-- | a    | add file, end of '/' for dir |
+-- | d    | delete file/dir              |
+-- | r    | rename file/dir              |
+-- | H    | Toggle hidden                |
+-- | o    | toggle expand/collapse       |
+-- | <cr> | enter cursored folder        |
+-- | R    | refresh tree list            |
+-- | q    | quit explore                 |
+-- |------+------------------------------|
 require('nvim-tree').setup {
   hijack_directories = {
     enable = true,
