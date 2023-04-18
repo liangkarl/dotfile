a57702e2e80f235aac129a99ebbc369d89ca1a88
karl.liang <karl.liang@quantatw.com>
2023-04-18 10:30:53 +0800

    nvim: change fuzzy finder interactively

--- config/nvim/configure.sh
+++ config/nvim/configure.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-config_dir=${XDG_CONFIG_HOME}
+config_dir=${XDG_CONFIG_HOME}/nvim
 
 echo "-- setup nvim config --"
 
+PS3='select a fuzzy finder above: '
+select opt in 'fzf' 'telescope' 'cancel'; do
+    if [[ "$opt" != 'cancel' ]]; then
+        echo "let g:fuzzy_finder = '$opt'" > ${config_dir}/config.vim
+    fi
+    break
+done
+echo "let g:editor_theme = 'material'" >> ${config_dir}/config.vim
+
 # link user-level config of EditorConfig
-ln -sf ${config_dir}/nvim/misc/editorconfig ~/.editorconfig
-ln -sf ${config_dir}/nvim/misc/clang-format ~/.clang-format
+ln -sf ${config_dir}/misc/editorconfig ~/.editorconfig
+ln -sf ${config_dir}/misc/clang-format ~/.clang-format
 
 nvim +"PlugInstall" <<< "Close the window after all of the plugins installed."
