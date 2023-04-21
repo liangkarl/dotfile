c4aa3580736fb90722a0f1d87700c0313106e3ed
karl.liang <karl.liang@quantatw.com>
2023-04-21 16:33:12 +0800

    enhancd: replace fuzzy finder support from skim to fzy

--- config/enhancd/bootstrap.sh
+++ config/enhancd/bootstrap.sh
@@ -8,7 +8,8 @@ export ENHANCD_ARG_DOUBLE_DOT='...'
 
 __enhancd_fzf_filter="fzf --preview 'ls -v --group-directories-first -N --color=auto -l --time-style=long-iso {}' --preview-window right,50% --height 35% --reverse --ansi"
 
-# there is a TUI issue for preview height in skim.
-__enhancd_skim_filter="sk --preview 'ls -v --group-directories-first -N --color=auto -l --time-style=long-iso {}' --preview-window right,50%  --reverse --ansi"
+# fzy for alternative usage
+__enhancd_fzy_filter="fzy -i"
+
 # config enhanced
-export ENHANCD_FILTER="$__enhancd_fzf_filter:$__enhancd_skim_filter"
+export ENHANCD_FILTER="$__enhancd_fzf_filter:$__enhancd_fzy_filter"
