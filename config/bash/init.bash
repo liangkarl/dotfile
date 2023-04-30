c8cd59dc4539aaa8f6ce35065b529acec3a8fa35
Karl Liang <liang.karl@outlook.com>
2023-04-30 11:43:45 +0800

    bash: add more XDG_* configurations

--- config/bash/init.bash
+++ config/bash/init.bash
@@ -77,11 +77,19 @@ __ps1_prompt() {
     __ps1_switch_form
 }
 
-# XDG Base Directory Specification
-# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
-[[ -z $XDG_CONFIG_HOME ]] && export XDG_CONFIG_HOME=${HOME}/.config
-[[ -z $XDG_DATA_HOME ]] && export XDG_DATA_HOME=${HOME}/.local/share
-[[ -z $XDG_CACHE_HOME ]] && export XDG_CACHE_HOME=${HOME}/.cache
+__xdg_config() {
+    # XDG Base Directory Specification
+    # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+    # https://wiki.archlinux.org/title/XDG_Base_Directory
+    export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+    export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+    export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+    export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
+    export XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
+    export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS:-/etc/xdg}"
+}
+
+__xdg_config
 
 # Import customized config
 # export SHELL_DIR
