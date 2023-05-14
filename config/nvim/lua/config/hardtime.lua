87b758c5e986a83adc077b20f9f71d83f6f9ba9a
Karl Liang <liang.karl@outlook.com>
2023-05-14 17:59:24 +0800

    nvim: add hardtime plugin to break up bad habbits

---
+++ config/nvim/lua/config/hardtime.lua
@@ -0,0 +1,16 @@
+-- Plugin: hardtime.nvim
+-- https://github.com/m4xshen/hardtime.nvim
+
+-- Talking about bad habbits in vim
+-- http://vimcasts.org/blog/2013/02/habit-breaking-habit-making/
+
+-- 1. Don’t use arrow keys and mouse.
+-- 2. Use relative jump (eg: 5k 12j) for vertical movement inside screen.
+-- 3. Use CTRL-U CTRL-D CTRL-B CTRL-F gg G for vertical movement outside screen.
+-- 4. Use word-motion (w W b B e E ge gE) for short distance horizontal movement.
+-- 5. Use f F t T 0 ^ $ , ; for mid long distance horizontal movement.
+-- 6. Use operator + motion/text-object (eg: ci{ d5j) whenever possible.
+require('hardtime').setup({
+  max_count = 4,
+  max_time = 300,
+})
