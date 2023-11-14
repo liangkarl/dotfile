9b34e8314812262e89454bfb3c4d1722ba38c9f8
Karl Liang <liang.karl@outlook.com>
2023-11-14 13:47:12 -0700

    nvim: resolve the warning msg of "deprecated show_buffer_default_icon"

--- config/nvim/config/bufferline.lua
+++ config/nvim/config/bufferline.lua
@@ -52,7 +52,9 @@ require('bufferline').setup {
     color_icons = true, -- whether or not to add the filetype icon highlights
     show_buffer_icons = true, -- disable filetype icons for buffers
     show_buffer_close_icons = false,
-    show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
+    get_element_icon = function(buf)
+      return require('nvim-web-devicons').get_icon(buf, {default = false})
+    end,
     show_close_icon = false,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
