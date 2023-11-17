e0c0327f571ba1c06690fa71a896e3eb1ecb5133
Karl Liang <liang.karl@outlook.com>
2023-11-16 17:15:38 -0700

    nvim: fix slow performance of Lua lsp
    The root cause is because we set entire lua files of neovim as library

--- config/nvim/config/nvim-lspconfig.lua
+++ config/nvim/config/nvim-lspconfig.lua
@@ -69,8 +69,6 @@ lsp.lua_ls.setup {
         globals = {'vim'},
       },
       workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
         checkThirdParty = false
       },
       -- Do not send telemetry data containing a randomized but unique identifier
