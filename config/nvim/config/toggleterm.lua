d759bdeb6e08569a0b9198d2b8e9ebe16ccb6fe7
Karl Liang <liang.karl@outlook.com>
2023-11-17 13:30:47 -0700

    nvim: enable and config toggleterm plugin

--- config/nvim/config/toggleterm.lua
+++ config/nvim/config/toggleterm.lua
@@ -1,1 +1,47 @@
-require("toggleterm").setup()
+require("toggleterm").setup({
+    -- size can be a number or function which is passed the current terminal
+  size = function(term)
+    if term.direction == "horizontal" then
+      return math.floor(vim.o.lines * 0.25)
+    elseif term.direction == "vertical" then
+      return math.floor(vim.o.columns * 0.7)
+    end
+  end,
+  direction = 'float',
+  auto_scroll = true,
+  float_opts = {
+    -- The border key is *almost* the same as 'nvim_open_win'
+    -- see :h nvim_open_win for details on borders however
+    -- the 'curved' border is a custom border type
+    -- not natively supported but implemented in this plugin.
+    border = 'curved',
+    width = function (_)
+      return math.floor(vim.o.columns * 0.7)
+    end,
+    height = function (_)
+      return math.floor(vim.o.lines * 0.7)
+    end,
+    winblend = 3,
+    zindex = 1,
+    -- TODO: alias vim as we want to open outside the toggleterm
+    -- on_open = function (term)
+    --   vim.cmd([[TermExec cmd='alias vim="nvr -l"']])
+    -- end
+  },
+
+  winbar = {
+    enabled = false,
+    name_formatter = function(term) --  term: Terminal
+      return term.name
+    end
+  },
+  hide_numbers = true, -- hide the number column in toggleterm buffers
+  autochdir = false, -- when neovim changes it current directory the terminal
+                     -- will change it's own when next it's opened
+  shell = vim.o.shell,
+})
+
+vim.cmd('nnoremap <leader>t <Cmd>exe v:count1 . "ToggleTerm"<cr>')
+vim.cmd('autocmd FileType toggleterm tnoremap <buffer> <C-d> <Cmd>ToggleTerm<cr>')
+vim.cmd('autocmd FileType toggleterm tnoremap <buffer> <Esc> <C-\\><C-n>')
+vim.cmd('autocmd FileType toggleterm nnoremap <buffer> <Esc> :ToggleTerm<cr>')
