a2dfa4e4d5a83f0a3bf329a191446196576acd23
Karl Liang <liang.karl@outlook.com>
2023-12-01 09:31:03 -0700

    nvim: adjust animation for performance

--- config/nvim/config/mini.lua
+++ config/nvim/config/mini.lua
@@ -1,22 +1,27 @@
+local animate
+
 require('mini.cursorword').setup()
 require('mini.bufremove').setup()
 require('mini.align').setup()
+
+animate = require('mini.indentscope').gen_animation
 require('mini.indentscope').setup({
   draw = {
     delay = 50,
+    animation = animate.none()
   }
 })
 require('mini.pairs').setup()
 
-local animate = require('mini.animate')
+animate = require('mini.animate')
 require('mini.animate').setup({
   cursor = {
     enable = false,
   },
   scroll = {
     enable = true,
-    timing = animate.gen_timing.linear({ duration = 50, unit = 'total' }),
-    subscroll = animate.gen_subscroll.equal({ max_output_steps = 10 }),
+    timing = animate.gen_timing.linear({ duration = 10, unit = 'step' }),
+    subscroll = animate.gen_subscroll.equal({ max_output_steps = 5 }),
   }
 })
 
