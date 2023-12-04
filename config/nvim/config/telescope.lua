426790f2384ffa2608b0c26850130a6c7d3789f7
Karl Liang <liang.karl@outlook.com>
2023-12-04 13:06:49 -0700

    nvim: add multiple selection support

--- config/nvim/config/telescope.lua
+++ config/nvim/config/telescope.lua
@@ -8,6 +8,23 @@ end
 local telescope = require('telescope')
 local opts = { noremap=true, silent=true }
 
+-- WA for multi-selections
+-- https://github.com/nvim-telescope/telescope.nvim/issues/1048
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 telescope.setup({
   defaults = {
     layout_strategy = 'vertical',
@@ -18,6 +35,11 @@ telescope.setup({
         width = 0.7,
       },
     },
+    mappings = {
+      i = {
+        ['<CR>'] = select_one_or_multi,
+      }
+    }
   },
 })
 
