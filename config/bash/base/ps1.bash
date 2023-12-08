290bca22b38c6a1166f16dad9b2ea7a97ae25ee5
Karl Liang <liang.karl@outlook.com>
2023-12-08 10:04:40 -0700

    bash: merge related files and simplify the directory

---
+++ config/bash/base/ps1.bash
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# In ANSI code, for example, '\e[0m' was used for reseting
+# '\e' means 'escape string'.
+# '[0' is 'function parameters'.
+# 'm' is 'function name'
+#
+# WARN:
+# '\[' and '\]' is the exception rule only for PS1 to add a ANSI section
+# if there is no esecape section, PS1 would become buggy
+
+__ps1_switch_form() {
+    local white='\[\e[01;38m\]'
+    local blue='\[\e[01;34m\]'
+    local yellow='\[\e[38;5;11m\]'
+    local green='\[\e[01;32m\]'
+    local purple='\[\e[38;5;63m\]'
+    local orange='\[\e[38;5;202m\]'
+    local sed_purple='\\\[\\\e[38;5;63m\\\]'
+    local sed_orange='\\\[\\\e[38;5;202m\\\]'
+    local reset='\[\e[0m\]'
+    local var
+
+    ps1_short() {
+        var='$(printf "'${orange}'%*s\r'${reset}''${white}'[\t] '${purple}'$? " \
+                $(( COLUMNS-1 )) "$(git branch 2>&- | sed -e "/^[^*]/d" -e "s/* \(.*\)/\1/")")'
+        var+=${green}'\u@\h '
+        var+=${blue}'\W '
+        var+=${purple}'${debian_chroot:+($debian_chroot) }'
+        var+=${yellow}'\$'${reset}' '
+
+        echo "$var"
+    }
+
+    ps1_long() {
+        var=${white}'[\t] ' # Current time
+        var+=${purple}'$? '
+        var+=${green}'\u@\h '
+        var+=${blue}'\w '
+        var+=${purple}'${debian_chroot:+($debian_chroot) }'
+        var+='$(git branch 2>&- | sed -e "/^[^*]/d" -e "s/* \(.*\)/'${sed_purple}'-> '${sed_orange}'\1/") '
+        var+=${reset}'\n└─ '
+        var+=${yellow}'\$'${reset}' '
+
+        echo "$var"
+    }
+
+
+    if (( __ps1_form == 0 )); then
+        PS1="$(ps1_long)"
+    else
+        PS1="$(ps1_short)"
+    fi
+
+    __ps1_form=$((__ps1_form ^ 0x1))
+}
+
+__ps1_form=1
+__ps1_switch_form
--- config/bash/init/fzf.sh
+++
@@ -1,9 +0,0 @@
-# Default command to use when input is tty
-# Now fzf (w/o pipe) will use fd instead of find
-export FZF_DEFAULT_COMMAND='fd --type f'
-
-# To apply the command to CTRL-T as well
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-
-# Default options
-# export FZF_DEFAULT_OPTS="--layout=reverse --inline-info"
