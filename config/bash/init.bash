169d2716404a78c8970cd1614c2eab999703ed95
Karl Liang <liang.karl@outlook.com>
2023-12-08 13:20:23 -0700

    bash: reorganize the call flow in init.bash

--- config/bash/init.bash
+++ config/bash/init.bash
@@ -1,8 +1,12 @@
-source $(dirname $BASH_SOURCE)/base/{ps1,xdg}.bash
+# set up XDG_xxx
+source $(dirname $BASH_SOURCE)/base/xdg.bash
 
 # Import customized config
 # export SHELL_DIR
-source ${XDG_CONFIG_HOME}/bash/config
+source ${XDG_CONFIG_HOME}/bash/env.bash
+
 grep -q -w "${HOME}/bin" <<< "$PATH" || PATH="${HOME}/bin:${PATH}"
+
+eval $(find $(dirname $BASH_SOURCE)/base -type f -exec 'echo' 'source' '{};' ';')
 
 eval $(find ${SHELL_DIR}/{completion,ext} -type f -exec 'echo' 'source' '{};' ';')
