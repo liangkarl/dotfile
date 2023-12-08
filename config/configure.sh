e39e8743a7c032a7db2f1d065ebb89c7f8400a8f
Karl Liang <liang.karl@outlook.com>
2023-12-08 12:28:49 -0700

    config: optimize git config

--- config/configure.sh
+++ config/configure.sh
@@ -14,13 +14,9 @@ export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-${HOME}/.config}
 export XDG_DATA_HOME=${XDG_DATA_HOME:-${HOME}/.local/share}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:-${HOME}/.cache}
 export OS="$(uname)"
-export GIT_DB="$(mktemp)"
 
 mydir="$(dirname $0)"
 conf_home=${XDG_CONFIG_HOME}
-
-echo "-- backup git config (global config) --"
-git config --global --list > $GIT_DB
 
 list=()
 if (( $# == 0 )); then
