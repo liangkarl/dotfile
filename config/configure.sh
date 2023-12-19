fee2b3626e01b8f6d06205828118d089f60475c0
Karl Liang <liang.karl@outlook.com>
2023-12-19 10:00:46 -0700

    bash: remove unnecessary os check

--- config/configure.sh
+++ config/configure.sh
@@ -13,7 +13,6 @@ fi
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-${HOME}/.config}
 export XDG_DATA_HOME=${XDG_DATA_HOME:-${HOME}/.local/share}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:-${HOME}/.cache}
-export OS="$(uname)"
 
 mydir="$(dirname $0)"
 conf_home=${XDG_CONFIG_HOME}
