ede55e2bb4d6de283863acfd9fe1e75fc5a37701
Karl Liang <liang.karl@outlook.com>
2024-01-09 22:52:50 -0700

    nvim: fix colorscheme issue for terminal

--- config/nvim/config/toggleterm.lua
+++ config/nvim/config/toggleterm.lua
@@ -38,7 +38,7 @@ require("toggleterm").setup({
   hide_numbers = true, -- hide the number column in toggleterm buffers
   autochdir = false, -- when neovim changes it current directory the terminal
                      -- will change it's own when next it's opened
-  shell = vim.o.shell,
+  shell = vim.fn.has('macunix') and string.format("%s -l", vim.o.shell) or vim.o.shell
 })
 
 -- Setup environmental variable
