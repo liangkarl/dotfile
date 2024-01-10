87a3b7b457c7d011a1c6b00e5bf376f73b37bcb6
Karl Liang <liang.karl@outlook.com>
2024-01-09 22:54:08 -0700

    nvim: sort source code

--- config/nvim/config/mini.lua
+++ config/nvim/config/mini.lua
@@ -1,8 +1,35 @@
-local animate
-
 require('mini.cursorword').setup()
 require('mini.bufremove').setup()
 require('mini.align').setup()
+require('mini.pairs').setup()
+
+local m
+
+m = require('mini.indentscope')
+m.setup({
+  draw = {
+    delay = 50,
+    animation = m.gen_animation.none()
+  }
+})
+
+m = require('mini.animate')
+m.setup({
+  scroll = {
+    enable = true,
+    timing = m.gen_timing.linear({
+      duration = 5,
+      unit = 'step'
+    }),
+    subscroll = m.gen_subscroll.equal({
+      max_output_steps = 4
+    }),
+  },
+  cursor = { enable = false, },
+  resize = { enable = false, },
+  open = { enable = false, },
+  close = { enable = false, },
+})
 
 require('mini.files').setup({
   window = {
@@ -10,43 +37,12 @@ require('mini.files').setup({
   }
 })
 
-animate = require('mini.indentscope').gen_animation
-require('mini.indentscope').setup({
-  draw = {
-    delay = 50,
-    animation = animate.none()
-  }
-})
-require('mini.pairs').setup()
-
-animate = require('mini.animate')
-require('mini.animate').setup({
-  scroll = {
-    enable = true,
-    timing = animate.gen_timing.linear({ duration = 5, unit = 'step' }),
-    subscroll = animate.gen_subscroll.equal({ max_output_steps = 4 }),
-  },
-  cursor = {
-    enable = false,
-    timing = animate.gen_timing.none(),
-  },
-  resize = {
-    enable = false,
-    timing = animate.gen_timing.none(),
-  },
-  open = {
-    enable = false,
-    timing = animate.gen_timing.none(),
-  },
-  close = {
-    enable = false,
-    timing = animate.gen_timing.none(),
-  },
+require('mini.trailspace').setup({
+  only_in_normal_buffers = true
 })
 
 -- NOTE:
 -- Low Performance when enabled in insert mode(?).
-require('mini.trailspace').setup({ only_in_normal_buffers = true })
 vim.cmd([[
   " WA for default environment setup
   lua MiniTrailspace.unhighlight()
