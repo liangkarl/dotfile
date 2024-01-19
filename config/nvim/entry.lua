a2a3c0edc1b6877ce3d699192f6f1063bf03efa4
Karl Liang <liang.karl@outlook.com>
2024-01-19 15:42:45 -0700

    nvim: fix keybind issue

--- config/nvim/entry.lua
+++ config/nvim/entry.lua
@@ -11,9 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- set <leader> key, otherwise there are some errors with the plugins
-vim.g.mapleader = ' '
-
 require("lazy").setup({
   -- Integration Development Environment
   { -- Start-up screen
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,15 @@
+--
+-- IMPORTANT NOTICE
+--
+-- PLACE CONFIGURATION HERE IF THEY ARE USED FOR GLOBAL AND MAY
+-- CAUSE DEPENDENCY PROBLEMS
+--
+
 local nvim_dir = vim.fn.expand('<sfile>:h')
 package.path = nvim_dir .. '/?.lua;' .. package.path
+
+-- set <leader> key ASAP to prevent dependency issue
+vim.g.mapleader = ' '
 
 vim.cmd("source " .. nvim_dir .. "/config.vim")
 vim.cmd("source " .. nvim_dir .. "/keybind.vim")
