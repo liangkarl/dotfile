7e0e2f4378de1826c19a7445326d3b664863cb2e
Karl Liang <liang.karl@outlook.com>
2024-01-28 16:13:29 -0700

    nvim: update sidebar config with new wrapper functions

---
+++ nvim/config/stickybuf.lua
@@ -0,0 +1,38 @@
+local m = require('helpers.utils')
+local gid = m.augroup('StickybufCtrl')
+
+return {
+  'stevearc/stickybuf.nvim',
+  config = function()
+    local s = require('stickybuf')
+
+    s.setup({})
+
+    m.autocmd("BufAdd", "*", function()
+        if vim.bo.modifiable then
+          return
+        end
+
+        if not s.is_pinned()
+            and vim.bo.filetype ~= "startify" then -- FIXME: WA for Startify here
+          s.pin()
+        end
+      end, {
+        desc = "Pin non-modifiable buffers",
+        group = gid
+    })
+
+    m.autocmd("BufDelete", "*", function()
+        if vim.bo.modifiable then
+          return
+        end
+
+        if s.is_pinned() then
+          s.unpin()
+        end
+      end, {
+        desc = "Unpin non-modifiable buffers",
+        group = gid
+    })
+  end
+}
