7fcc771d227bf1083aac561ac7f62df9bcf6c5b4
Karl Liang <liang.karl@outlook.com>
2024-01-31 10:55:06 -0700

    nvim: update symbol manager related configs
    1. Update aerial config
    2. Update outline config
    3. Switch to aerial

--- nvim/config/outline.lua
+++ nvim/config/outline.lua
@@ -1,14 +1,21 @@
-require('outline').setup({
-  symbols = {
-    filter = {
-      "Class",
-      "Function",
-      "Module",
-      "Method",
-      "Struct",
-    },
-  }
-})
+local m = require('helpers.utils')
 
-vim.keymap.set('n', '<leader>sm', ':Outline<cr>')
-vim.keymap.set('n', '<leader>si', ':OutlineStatus<cr>')
+return { -- Symbol Manager
+  'hedyhli/outline.nvim',
+  config = function()
+    require('outline').setup({
+      symbols = {
+        filter = {
+          "Class",
+          "Function",
+          "Module",
+          "Method",
+          "Struct",
+        },
+      }
+    })
+
+    m.noremap('n', '<leader>sm', '<cmd>Outline<cr>', 'Toggle Outline Symbol Manager')
+    m.noremap('n', '<leader>si', '<cmd>OutlineStatus<cr>', 'Get Outline Symbol Manager info')
+  end
+}
