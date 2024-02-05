fe3a793019df9fd25044b8fe16145710e4e81e97
Karl Liang <liang.karl@outlook.com>
2024-02-05 00:05:17 -0700

    nvim: extend keymap function

--- nvim/helpers/utils.lua
+++ nvim/helpers/utils.lua
@@ -1,11 +1,18 @@
 local M = {}
 
-M.map = function(mode, lhs, rhs, desc)
-  vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
+M.map = function(mode, lhs, rhs, desc, opt)
+  local opts = opt or {}
+
+  opts.silent = true
+  opts.desc = desc
+  vim.keymap.set(mode, lhs, rhs, opts)
 end
 
-M.noremap = function(mode, lhs, rhs, desc)
-  vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, noremap = true })
+M.noremap = function(mode, lhs, rhs, desc, opt)
+  local opts = opt or {}
+
+  opts.noremap = true
+  M.map(mode, lhs, rhs, desc, opts)
 end
 
 M.autocmd = function(event, pattern, action, opt)
