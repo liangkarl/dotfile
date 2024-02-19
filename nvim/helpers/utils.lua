545cae156cb4c43b28396a224ecf197fba97f63f
Karl Liang <liang.karl@outlook.com>
2024-02-18 23:19:12 -0700

    nvim: refactor utils..lua and add m.command

--- nvim/helpers/utils.lua
+++ nvim/helpers/utils.lua
@@ -1,22 +1,21 @@
 local M = {}
+local api = vim.api
 
-M.map = function(mode, lhs, rhs, desc, opt)
-  local opts = opt or {}
-
-  opts.silent = true
+M.map = function(mode, lhs, rhs, desc, opts)
+  opts = opts or {}
+  opts.silent = opts.silent or true
   opts.desc = desc
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
-M.noremap = function(mode, lhs, rhs, desc, opt)
-  local opts = opt or {}
-
+M.noremap = function(mode, lhs, rhs, desc, opts)
+  opts = opts or {}
   opts.noremap = true
   M.map(mode, lhs, rhs, desc, opts)
 end
 
-M.autocmd = function(event, pattern, action, opt)
-  local opts = opt or {}
+M.autocmd = function(event, pattern, action, opts)
+  opts = opts or {}
 
   opts.pattern = pattern
   if type(action) == "string" then
@@ -25,15 +24,24 @@ M.autocmd = function(event, pattern, action, opt)
     opts.callback = action
   end
 
-  return vim.api.nvim_create_autocmd(event, opts)
+  return api.nvim_create_autocmd(event, opts)
 end
 
-M.augroup = function(name)
-  return vim.api.nvim_create_augroup(name, { clear = true })
+M.augroup = function(name, opts)
+  opts = opts or {}
+  opts.clear = opts.clear or true
+  return api.nvim_create_augroup(name, opts)
 end
 
 M.highlight = function(name, opts)
-  return vim.api.nvim_set_hl(0, name, opts)
+  opts = opts or {}
+  return api.nvim_set_hl(opts.buffer or 0, name, opts)
+end
+
+M.command = function(cmd, action, opts)
+  opts = opts or {}
+  opts.bang = opts.bang or true
+  return api.nvim_create_user_command(cmd, action, opts)
 end
 
 return M
