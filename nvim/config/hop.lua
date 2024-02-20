0f52f6b495ddc067d2e426a0304889c399a48911
Karl Liang <liang.karl@outlook.com>
2024-02-19 23:28:54 -0700

    nvim: add hop.nvim to replace easymotion.nvim

---
+++ nvim/config/hop.lua
@@ -0,0 +1,7 @@
+return {
+  'phaazon/hop.nvim',
+  config = function()
+    -- you can configure Hop the way you like here; see :h hop-config
+    require('hop').setup({ keys = 'jfkdienvmchgurlsytow;apq' })
+  end
+}
