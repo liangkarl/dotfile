20535fe4bc57a85031fb367dcb01d045e889fec6
Karl Liang <liang.karl@outlook.com>
2024-03-21 00:08:39 -0600

    enhancd: update configuration
    1. update keywords '...' and '--'
    2. optimize the script

--- enhancd/bootstrap.sh
+++ enhancd/bootstrap.sh
@@ -2,14 +2,21 @@
 
 # https://github.com/b4b4r07/enhancd
 # The ENHANCD_DIR variable is a base directory path. It defaults to ~/.enhancd.
-export ENHANCD_DIR="$XDG_CONFIG_HOME/enhancd"
-# don't override `cd ..` by default
-export ENHANCD_ARG_DOUBLE_DOT='...'
 
-__enhancd_fzf_filter='fzf --height 35% --reverse'
+__enhancd_bootstrap() {
+    local fzy_filter fzf_filter
 
-# fzy for alternative usage
-__enhancd_fzy_filter="fzy -i"
+    fzf_filter='fzf --height 35% --reverse'
+    fzy_filter="fzy -i"
+    export ENHANCD_FILTER="$fzf_filter:$fzy_filter"
 
-# config enhanced
-export ENHANCD_FILTER="$__enhancd_fzf_filter:$__enhancd_fzy_filter"
+    export ENHANCD_ENABLE_SINGLE_DOT=false   # Disable sub directories search
+    export ENHANCD_ENABLE_HOME=false         # WA for disabling `cd `
+    export ENHANCD_ARG_DOUBLE_DOT='...'      # List parent directories
+    export ENHANCD_ARG_HYPHEN='--'           # List 10 latest directories
+    export ENHANCD_ARG_HOME='='              # List change
+    export ENHANCD_DIR="$XDG_CONFIG_HOME/enhancd"
+}
+
+__enhancd_bootstrap
+
