80b7b49af460754ebd81dd6dfb52aa3530fcca06
Karl Liang <liang.karl@outlook.com>
2024-03-21 23:01:41 -0600

    all: adjust output messages

--- package.mk
+++ package.mk
@@ -11,14 +11,14 @@ $(LIST):
 	$(H)$(eval SRC_DIR:=$(TOP)/$@)
 	$(H)$(eval DST_DIR:=$(CONF_HOME)/$@)
 
-	$(H)echo "Install configuration: $@"
+	$(H)echo "-- $@: copy files --"
 	$(H)cp -rvf $(SRC_DIR) $(CONF_HOME)/
-	$(H)echo "-------------------------------"
 	$(H)if [[ -e $(DST_DIR)/Makefile ]]; then
 		H=$(H) make -C $(DST_DIR)
 	elif [[ -e $(DST_DIR)/configure.sh ]]; then
 		eval $(DST_DIR)/configure.sh
 	fi
+	$(H)echo ""
 
 top: procps
 
