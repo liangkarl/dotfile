e03ec51ef9230a11422d9b8e7f0d2ec985a1681d
Karl Liang <liang.karl@outlook.com>
2024-04-06 15:01:58 -0600

    bash: replace configure.sh with Makefile and simplify the script

--- bash/init.bash
+++ bash/init.bash
@@ -1,10 +1,6 @@
 # set up XDG_xxx
 source $(dirname $BASH_SOURCE)/base/xdg.bash
 
-# Import customized config
-# export SHELL_DIR
-source ${XDG_CONFIG_HOME}/bash/env.bash
-
 grep -q -w "${HOME}/bin" <<< "$PATH" || PATH="${HOME}/bin:${PATH}"
 
 eval $(find $(dirname $BASH_SOURCE)/base -type f -exec 'echo' 'source' '{};' ';')
