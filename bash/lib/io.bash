1eeb3ce38f47c13938197e70cde50fa95b1c5abc
Karl Liang <liang.karl@outlook.com>
2024-06-19 20:20:54 +0000

    bash: fix the render bug of not using quotes

--- bash/lib/io.bash
+++ bash/lib/io.bash
@@ -2,22 +2,22 @@
 
 if (( $(tput colors) == 256 )); then
 	# print text with green color
-	pr_good() { printf -- "\033[1;32m$@\033[0m\n"; }
+	pr_good() { printf -- "\033[1;32m$*\033[0m\n"; }
 	# print text with white color
-	pr_hint() { printf -- "\033[1;37m$@\033[0m\n"; }
+	pr_hint() { printf -- "\033[1;37m$*\033[0m\n"; }
 	# print text with yellow color
-	pr_warn() { printf -- "\033[1;33m$@\033[0m\n"; }
+	pr_warn() { printf -- "\033[1;33m$*\033[0m\n"; }
 	# print text with red color
-	pr_err() { printf -- "\033[1;31m$@\033[0m\n"; }
+	pr_err() { printf -- "\033[1;31m$*\033[0m\n"; }
 else
-	pr_good() { printf -- "$@\n"; }
-	pr_hint() { printf -- "$@\n"; }
-	pr_warn() { printf -- "$@\n"; }
-	pr_err() { printf -- "$@\n"; }
+	pr_good() { printf -- "$*\n"; }
+	pr_hint() { printf -- "$*\n"; }
+	pr_warn() { printf -- "$*\n"; }
+	pr_err() { printf -- "$*\n"; }
 fi
 
-pr_bad() { pr_err $@; }
-pr_info() { printf -- "$@\n"; }
+pr_bad() { pr_err $*; }
+pr_info() { printf -- "$*\n"; }
 
 # !0: error out
 # 0: normal out
@@ -29,9 +29,9 @@ exit_msg() {
 	shift
 
 	if (( $r != 0 )); then
-		pr_err $@
+		pr_err "$*"
 	else
-		pr_info $@
+		pr_info "$*"
 	fi
 
 	exit $r
