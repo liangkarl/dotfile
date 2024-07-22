e810cb83d7f7daa1b882ea3a9fe5b040b492ec55
Karl Liang <liang.karl@outlook.com>
2024-07-22 16:46:29 +0000

    bash: add new debug lib for bash

--- bash/lib/sys.bash
+++ bash/lib/sys.bash
@@ -5,3 +5,21 @@
 chk_ver() {
 	[  "$1" = "$(printf -- "$1\n$2\n" | sort -V | head -n1)" ]
 }
+
+backtrace() {
+	TRACE=""
+	CP=$$ # PID of the script itself [1]
+
+	while true # safe because "all starts with init..."
+	do
+			CMDLINE=$(cat /proc/$CP/cmdline)
+			PP=$(grep PPid /proc/$CP/status | awk '{ print $2; }') # [2]
+			TRACE="$TRACE [$CP]:$CMDLINE\n"
+			if [ "$CP" == "1" ]; then # we reach 'init' [PID 1] => backtrace end
+					break
+			fi
+			CP=$PP
+	done
+	echo "Backtrace of '$0'"
+	echo -en "$TRACE" | tac | grep -n ":" # using tac to "print in reverse" [3]
+}
