156e16f0c85539d85b351bfa694ca31416613249
Karl Liang <liang.karl@outlook.com>
2024-10-01 22:02:08 +0000

    nvim: integrate trouble into telescope

--- nvim/config/trouble.lua
+++ nvim/config/trouble.lua
@@ -1,5 +1,24 @@
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = true
+  config = function()
+    local actions = require("telescope.actions")
+    local open_with_trouble = require("trouble.sources.telescope").open
+
+    -- Use this to add more results without clearing the trouble list
+    local add_to_trouble = require("trouble.sources.telescope").add
+
+    local telescope = require("telescope")
+
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = { ["<c-t>"] = open_with_trouble },
+          n = { ["<c-t>"] = open_with_trouble },
+        },
+      },
+    })
+
+    require("trouble").setup()
+  end
 }
