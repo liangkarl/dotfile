d7612f4f396c3389522f44ccdf290a1f57f0e9a6
Karl Liang <liang.karl@outlook.com>
2024-11-27 17:23:07 +0000

    nvim: update options.lua

--- nvim/options.lua
+++ nvim/options.lua
@@ -1,62 +1,87 @@
 local opt = vim.o -- different from vim.opt
+local g = vim.g
 
-opt.wrap = true
-opt.wrapmargin = 8 -- wrap lines when coming within n characters from side
-
+-------------------------------------------------
+--- General
+-------------------------------------------------
 opt.termguicolors = true
--- set wait time for combined keys
-opt.timeoutlen = 300
--- Fix backspace indent
-opt.backspace = "indent,eol,start"
+opt.timeoutlen = 300         -- set wait time for combined keys
+opt.wildmode = "full"        -- Tab completion in command bar
+opt.wildignore = "*.o,*.obj,.git,*.rbc,.pyc,__pycache__"
+-- Symbols for non-charactors:
+-- tab:→\ , space:·, nbsp:␣, trail:•, eol:¶, precedes:«, extends:»
+opt.listchars = "tab:→ ,nbsp:␣,precedes:«,extends:»"
+opt.bomb = true              -- Set Byte Order Mask(BOM) dealing with UTF8 in window
+opt.equalalways = false      -- Close a split window in Vim without resizing other windows
+opt.mouse = ""               -- disable support mouse action in normal mode.
+opt.diffopt = opt.diffopt .. ",algorithm:histogram"
+opt.display = opt.display .. ",uhex" -- Show unprintable characters hexadecimal as <xx> instead of using ^C and ~C.
+opt.scrolljump = 5
+
+-------------------------------------------------
+--- File
+-------------------------------------------------
+opt.backup = false
+opt.writebackup = false      -- When vim failed to write buffer, don't create backup-like file for it.
+opt.swapfile = false
+opt.binary = true            -- treat all files as binary to prevent from unexpected changes
 -- SHAre DAta
 -- Better modes.  Remeber where we are, support yankring
 opt.shada = "!,'100,\"100,:20,<50,s10,h,n~/.vim.shada"
 
--- Set the title text in the window
-opt.title = true
+-------------------------------------------------
+--- Neovim UI
+-------------------------------------------------
+opt.wrap = true
+opt.wrapmargin = 8           -- wrap lines when coming within n characters from side
+opt.title = true             -- Set the title text in the window
 opt.titlestring = "%F"
-
--- Tab completion in command bar
-opt.wildmode = "full"
-opt.wildignore = "*.o,*.obj,.git,*.rbc,.pyc,__pycache__"
-
--- Symbols for non-charactors:
--- tab:→\ , space:·, nbsp:␣, trail:•, eol:¶, precedes:«, extends:»
-opt.listchars = "tab:→ ,nbsp:␣,precedes:«,extends:»"
-
--- Use modeline overrides
-opt.modeline = true
+opt.backspace = "indent,eol,start" -- Fix backspace indent
+opt.modeline = true          -- Use modeline overrides
 opt.modelines = 10
-
--- Set Byte Order Mask(BOM) dealing with UTF8 in window
-opt.bomb = true
--- treat all files as binary to prevent from unexpected changes
-opt.binary = true
-
--- show the line numbers with hybrid mode in the left side
-opt.number = true
+opt.number = true            -- show the line numbers with hybrid mode in the left side
 opt.relativenumber = true
-
--- Close a split window in Vim without resizing other windows
-opt.equalalways = false
-
--- Set up font for special characters
-opt.guifont = "SauceCodePro Nerd Font Mono"
-
-opt.diffopt = opt.diffopt .. ",algorithm:histogram"
-
-opt.backup = false
--- When vim failed to write buffer, don't create backup-like file for it.
-opt.writebackup = false
-opt.swapfile = false
-
--- Don't show mode text(eg, INSERT) as lualine has already do it
-opt.showmode = false
-
--- Show unprintable characters hexadecimal as <xx> instead of using ^C and ~C.
-opt.display = opt.display .. ",uhex"
-
--- disable support mouse action in normal mode.
-opt.mouse = ""
-
+opt.guifont = "SauceCodePro Nerd Font Mono" -- Set up font for special characters
 opt.cursorline = true
+opt.showmode = false         -- Don't show mode text(eg, INSERT) as lualine has already do it
+
+-- https://superuser.com/questions/1202889/vim-syntax-highlight-limited-to-3000-chars
+opt.synmaxcol = 0
+opt.showmatch = true
+-- vim.cmd("syntax sync fromstart")
+
+-----------------------------------------------------------
+-- Startup
+-----------------------------------------------------------
+-- Disable builtin plugins
+local disabled_built_ins = {
+   "2html_plugin",
+   "getscript",
+   "getscriptPlugin",
+   "gzip",
+   "logipat",
+   "netrw",
+   "netrwPlugin",
+   "netrwSettings",
+   "netrwFileHandlers",
+   "matchit",
+   "tar",
+   "tarPlugin",
+   "rrhelper",
+   "spellfile_plugin",
+   "vimball",
+   "vimballPlugin",
+   "zip",
+   "zipPlugin",
+   "tutor",
+   "rplugin",
+   "synmenu",
+   "optwin",
+   "compiler",
+   "bugreport",
+   "ftplugin",
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+   g["loaded_" .. plugin] = 1
+end
