3fdf8290edd4cd9f09299136e876c096b232e165
Karl Liang <karl.liang@microhardcorp.com>
2025-01-23 15:43:55 +0000

    Makefile: Simplified the copy process, don't use link

--- tool/configure.sh
+++ tool/configure.sh
@@ -15,18 +15,10 @@ mydir="$(dirname $0)"
 pfx="$(uname -s)-$(uname -m)"
 pfx="${pfx//aarch64/arm64}"
 
-home_bin="${HOME}/bin"
+local_bin="${HOME}/.local/bin"
 toolkit="${XDG_CONFIG_HOME}/toolkit"
-toolkit_bin="$toolkit/bin"
 toolkit_script="$toolkit/script"
 
-echo "-- Clean $toolkit --"
-
-rm -rf $toolkit
-
-echo "-- Copy tools to $toolkit --"
-
-mkdir $toolkit
 cp -rf $mydir/{bin,script} $toolkit
 if [[ -n "$SHELL_DIR" ]]; then
     grep -q "SCRIPT_DIR" $SHELL_DIR/config \
@@ -35,35 +27,15 @@ else
     echo "\$SHELL_DIR does not exist"
 fi
 
+# copy bin/ script/ to toolkit
+[[ ! -d "$local_bin" ]] && mkdir $local_bin
+
 for file in $(ls $mydir/prebuild); do
-    cp -f $mydir/prebuild/$file/$file-${pfx,,} $toolkit_bin/$file || true
+    cp -f $mydir/prebuild/$file/$file-${pfx,,} $local_bin/$file || true
 done
 
-# create links between $HOME/bin and $toolkit
-echo "-- Link $toolkit to $home_bin --"
-
-# copy bin/ script/ to toolkit
-[[ ! -d "$home_bin" ]] && mkdir $home_bin
-
-# get the full file path of the files in toolkit
-list="$(find ${toolkit_bin} -type f -exec 'readlink' '-e' '{}' ';')"
-
-# clean invailid links in home_bin
-find $home_bin -type l ! -exec test -e {} \; -delete
-
-for file in ${list[@]}; do
-    link_pname=${home_bin}/$(basename $file)
-
-    # clean non-link or invalid link
-    if [[ ! -h ${link_pname} ]]; then
-        rm -f ${link_pname}
-
-    # clean the mislead link
-    elif [[ "$(readlink ${link_pname})" != ${file} ]]; then
-        rm -f ${link_pname}
-    fi
-
-    ln -sf $file $home_bin
+for file in $(ls $mydir/bin); do
+    cp -f $mydir/bin/$file $local_bin/$file || true
 done
 
 # create special files in $HOME/bin
@@ -71,5 +43,8 @@ fake="$(readlink -e ${toolkit_script}/fake)"
 while IFS= read -r file
 do
     # don't force override as this is a 'fake' file
-    ln -s $fake $home_bin/$(basename $file) || true
+    ln -s $fake $local_bin/$(basename $file) || true
 done < $mydir/fake.list
+
+echo "-- Clean $toolkit --"
+rm -rf $toolkit
