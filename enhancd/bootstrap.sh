68b382970763376f5ea3172ea7838aa9d84c966b
Karl Liang <karl.liang@microhardcorp.com>
2025-02-05 16:28:06 +0000

    enhancd: enable sub dir search and change the keybinding for last 10 dir

--- enhancd/bootstrap.sh
+++ enhancd/bootstrap.sh
@@ -10,11 +10,12 @@ __enhancd_bootstrap() {
     fzy_filter="fzy -i"
     export ENHANCD_FILTER="$fzf_filter:$fzy_filter"
 
-    export ENHANCD_ENABLE_SINGLE_DOT=false   # Disable sub directories search
+    export ENHANCD_ENABLE_SINGLE_DOT=true    # Disable sub directories search
     export ENHANCD_ENABLE_HOME=false         # WA for disabling `cd `
     export ENHANCD_ARG_DOUBLE_DOT='...'      # List parent directories
-    export ENHANCD_ARG_HYPHEN='--'           # List 10 latest directories
-    export ENHANCD_ARG_HOME='='              # List change
+    export ENHANCD_ARG_HYPHEN='='            # List 10 latest directories
+    export ENHANCD_HYPHEN_NUM=20             # List recent directory entries
+    export ENHANCD_ARG_HOME=''               # List change
     export ENHANCD_DIR="$XDG_CONFIG_HOME/enhancd"
 }
 
