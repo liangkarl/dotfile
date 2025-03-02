0a2722b0cb225c53c759b906dbdd4f05ff0e7b1b
Karl Liang <liang.karl@outlook.com>
2025-03-01 17:36:23 -0700

    bash: fix issue on Mac

--- bash/lib/devel.bash
+++ bash/lib/devel.bash
@@ -1,39 +1,45 @@
 #!/usr/bin/env bash
 
 path.uniq() {
-    echo "${*%/}" | tr -s /
+    path.abs "$*"
 }
 
 path.abs() {
-    realpath -m -s "$@"
-}
+    realpath -m -s "$@" || \
+        grealpath -m -s "$@"
+} 2>&-
 
 path.rel() {
-    realpath -m -s --relative-to=$(pwd) "$@"
-}
+    realpath -m -s --relative-to=$(pwd) "$@" || \
+        grealpath -m -s --relative-to=$(pwd) "$@"
+} 2>&-
 
 # call the func to get the file path inside the sourced script
-path.in_source() { echo "$(readlink -e "$(dirname ${BASH_SOURCE[0]})")"; }
+path.in_source() {
+    readlink -e "$(dirname ${BASH_SOURCE[0]})" || \
+        greadlink -e "$(dirname ${BASH_SOURCE[0]})"
+} 2>&-
 
 # usage:
-# path.load FILE [PATH]
+# path.load FILE
 path.load() {
-    local path
+    local path name
 
-    path="$2"
+    name="$(basename $1)"
+    path="$(dirname $1)"
     [[ -z "$path" ]] && path=$(pwd)
     path="$(path.abs $path)"
 
-	while [[ ! -e ${path}/$1 ]]; do
-        [[ -z "${path}" ]] && echo "$1" && break 1
+    while [[ ! -e ${path}/$name ]]; do
+        [[ -z "${path}" ]] && return 1
         path="${path%/*}"
-	done
+    done
 
-    echo ${path}/$1
+    echo ${path}/$name
 }
 
 source.load() {
-    source $(path.load $*)
+    source $(path.load $* || echo "$*")
 }
 
 msg.exit() {
@@ -61,31 +67,31 @@ is_hex()  { ((16#$1)) &> /dev/null || return 1; }
 # chk_ver [min-ver] [cur-ver]
 # return 0 if current ver >= min ver.
 vercmp() {
-	[  "$1" = "$(printf -- "$1\n$2\n" | sort -V | head -n1)" ]
+    [  "$1" = "$(printf -- "$1\n$2\n" | sort -V | head -n1)" ]
 }
 
 dump_stack() {
-	local TRACE=""
-	local CP=${1:-$$} # PID of the script itself [1]
+    local TRACE=""
+    local CP=${1:-$$} # PID of the script itself [1]
 
-	while true # safe because "all starts with init..."
-	do
-		CMDLINE=$(cat /proc/$CP/cmdline)
-		PP=$(grep PPid /proc/$CP/status | awk '{ print $2; }') # [2]
-		TRACE="$TRACE [$CP]:$CMDLINE\n"
-		if [ "$CP" == "1" ]; then # we reach 'init' [PID 1] => backtrace end
-			break
-		fi
-		CP=$PP
-	done
-	echo "Backtrace of '$0'"
-	echo -en "$TRACE" | tac | grep -n ":" # using tac to "print in reverse" [3]
+    while true # safe because "all starts with init..."
+    do
+        CMDLINE=$(cat /proc/$CP/cmdline)
+        PP=$(grep PPid /proc/$CP/status | awk '{ print $2; }') # [2]
+        TRACE="$TRACE [$CP]:$CMDLINE\n"
+        if [ "$CP" == "1" ]; then # we reach 'init' [PID 1] => backtrace end
+            break
+        fi
+        CP=$PP
+    done
+    echo "Backtrace of '$0'"
+    echo -en "$TRACE" | tac | grep -n ":" # using tac to "print in reverse" [3]
 }
 
 # pause [ret]
 pause() {
-	read -p "Press ENTER to continue."
-	return $1
+    read -p "Press ENTER to continue."
+    return $1
 }
 
 #msg.err()
