1761545b635a5253e03d8d1a29f86e48b715ac5d
Karl Liang <karl.liang@microhardcorp.com>
2025-03-10 16:38:50 +0000

    bash: reuse source.dir

--- bash/init.bash
+++ bash/init.bash
@@ -49,7 +49,7 @@ lib.add devel
 # source bash/env/11_xdg.bash;
 # source bash/env/12_ps1.bash;
 # ...
-eval $(find ${SHELL_DIR}/init -type f -exec 'echo' 'source' '{};' ';' | sort)
+source.dir "${SHELL_DIR}/init"
 
 msg.dbg "path: $PATH"
 msg.dbg "completed"
