b6d84b781e0ef4ff367923f372ac159f05863247
Karl Liang <liang.karl@outlook.com>
2025-03-15 15:13:00 -0600

    nvim: remove typo

--- nvim/core/lazy.lua
+++ nvim/core/lazy.lua
@@ -32,7 +32,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Use a protected call so we don't error out on first use
-local status_ok, lazy = pcall(require, 'lazy')require('lazy')
+local status_ok, lazy = pcall(require, 'lazy')
 if not status_ok then
   return
 end
