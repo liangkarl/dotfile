be26694b77d154c9b86bccc8e5d0ad5925c9bab9
Karl Liang <karl.liang@microhardcorp.com>
2025-03-27 08:34:10 -0600

    bash: modify ps1

--- bash/init/12_ps1.bash
+++ bash/init/12_ps1.bash
@@ -12,58 +12,82 @@
 __ps1_switch_form() {
     lib.load ansi
 
-    local w='\['$(ansi 111)'\]'
-    local b='\['$(ansi 001)'\]'
-    local y='\['$(ansi 110)'\]'
-    local g='\['$(ansi 010)'\]'
-    local purple='\[\e[38;5;63m\]'
-    local orange='\[\e[38;5;202m\]'
-    local sed_purple='\\\[\\\e[38;5;63m\\\]'
-    local sed_orange='\\\[\\\e[38;5;202m\\\]'
-
     __ps1_git_branch() {
         git branch 2> /dev/null | sed -n "/\* /s/^\* \(.*\)$/\1 /p"
     }
 
+    __wrap() {
+        echo '\['$1'\]'
+    }
+
+    __ps1_tab() {
+        echo -e '\t'
+    }
+
+    __ps1_pos() {
+        local br dir offset
+        br="$(__ps1_git_branch)"
+        dir="$(basename $(pwd))"
+        offset=15 # '@' + ' '*5 + '$' + TIME(8) + ' '*4
+        echo $((${#USER} + ${#HOSTNAME} + ${#br} + ${#dir} + offset))
+    }
+
+    local w=$(__wrap "$(ansi 111)")
+    local b=$(__wrap "$(ansi 001)")
+    local y=$(__wrap "$(ansi 110)")
+    local g=$(__wrap "$(ansi 010)")
+    local rs=$(__wrap "$_RS")
+    local grey=$(__wrap "$(ansi_tc 222222)")
+    local purple=$(__wrap "$(ansi_256 115)")
+    local orange=$(__wrap "$(ansi_256 510)")
+
     ps1_short() {
         local var
-        var+=${_RS}${purple}'\!:\j:$? '
+        var+=${purple}'\t '
         var+=${g}'\u@\h '
         var+=${b}'\W'
         var+=${purple}'${debian_chroot:+(:$debian_chroot)} '
-        # var+='$(git branch 2>&- | sed -n "/\* /s/^\* \(.*\)$/'${sed_purple}':'${sed_orange}'\1/p")'
         var+=${orange}'$(__ps1_git_branch)'
-        var+=${y}'\$'${_RS}' '
+        var+=${y}'\$'${rs}' '
         echo "$var"
     }
 
     ps1_long() {
         local var
-        var=${_RS}${w}'[\t] ' # Current time
-        var+=${purple}'\!:\j:$? '
-        var+=${g}'\u@\h '
-        var+=${b}'\w '
-        var+=${purple}'${debian_chroot:+($debian_chroot) }'
-        # var+='$(git branch 2>&- | sed -n "/\* /s/^\* \(.*\)$/'${sed_purple}'-> '${sed_orange}'\1/p") '
-        var+=${orange}'$(__ps1_git_branch)'
-        var+=${_RS}'\n└─ '
-        var+=${y}'\$'${_RS}' '
+        var=${rs}${w}'[\t] ' # Current time
+        var+=${purple}'\[\!:\j:$? \]'
+        var+=${g}'\[\u@\h \]'
+        var+=${b}'\[\w \]'
+        var+=${purple}'\[${debian_chroot:+($debian_chroot) }\]'
+        var+=${orange}'\[$(__ps1_git_branch)\]\[\r\]'
+        var+=${rs}'\[\n\r\] '
+        var+=${y}'\$'${rs}' '
 
         echo "$var"
     }
 
-
     if (( __ps1_form == 0 )); then
         PS1="$(ps1_long)"
     else
-        # https://unix.stackexchange.com/questions/252229/ps1-prompt-to-show-elapsed-time
+        # https://unix.stackexchange.com/questions/252228/ps1-prompt-to-show-elapsed-time
         PS1[3]=$SECONDS
-        PS1='[${PS1[!(PS1[1]=!1&(PS1[3]=(PS1[2]=$SECONDS-${PS1[3]})/3600))]#${PS1[3]%%*??}0}$((PS1[3]=(PS1[2]/60%60),${PS1[3]})):${PS1[1]#${PS1[3]%%*??}0}$((PS1[3]=(PS1[2]%60),${PS1[3]})):${PS1[1]#${PS1[3]%%*??}0}$((PS1[3]=(SECONDS),${PS1[3]}))] '
+        # https://www.gnu.org/software/termutils/manual/termutils-2.0/html_chapter/tput_1.html
+        PS1='\[$(tput cuf $(__ps1_pos))\]'
+        PS1+=$grey
+        PS1+='\['
+        PS1+='      <<  '
+        PS1+='RET:$?  '
+        PS1+='JOB:\j  '
+        PS1+='${PS1[!(PS1[1]=!1&(PS1[3]=(PS1[2]=$SECONDS-${PS1[3]})/3600))]#${PS1[3]%%*??}0}$((PS1[3]=(PS1[2]/60%60),${PS1[3]})):${PS1[1]#${PS1[3]%%*??}0}$((PS1[3]=(PS1[2]%60),${PS1[3]})):${PS1[1]#${PS1[3]%%*??}0}$((PS1[3]=(SECONDS),${PS1[3]}))  '
+        PS1+='CMD#:\!'
+        PS1+='\r'
+        PS1+='\]'
         PS1+="$(ps1_short)"
     fi
 
     __ps1_form=$((__ps1_form ^ 0x1))
 
+    unset __wrap
     lib.unload ansi
 }
 
