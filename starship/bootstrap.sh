7b8616026786cc28d891490255d4ae33ba156659
Karl Liang <liang.karl@outlook.com>
2025-03-29 01:19:05 -0600

    starship: add blerc

--- starship/bootstrap.sh
+++ starship/bootstrap.sh
@@ -4,8 +4,18 @@
 # The STARSHIP_CONFIG variable is a base directory path. It defaults to ~/.enhancd.
 
 __starship_bootstrap() {
-    export STARSHIP_CONFIG=$(dirname ${BASH_SOURCE[0]})/starship.toml
+    local dir
+
+    dir=$(dirname ${BASH_SOURCE[0]})
+
+    # init ble.sh
+    source ${dir}/ble.sh/out/ble.sh --noattach --rcfile ${dir}/blerc
+
+    # init starship
+    export STARSHIP_CONFIG=${dir}/starship.toml
     eval "$(starship init bash)"
+
+    [[ ! ${BLE_VERSION-} ]] || ble-attach
 }
 
 oneshot __starship_bootstrap
