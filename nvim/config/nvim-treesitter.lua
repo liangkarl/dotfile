da5d11c677a5e42b10779d6545ca742cbeb8dbbe
Karl Liang <karl.liang@microhardcorp.com>
2025-04-08 11:24:17 -0600

    nvim: update treesitter

--- nvim/config/nvim-treesitter.lua
+++ nvim/config/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return { -- Syntax highlight/lint with `treesitter`
   -- NOTE: treesitter might have to update after updating neovim
   'nvim-treesitter/nvim-treesitter',
   build = ":TSUpdateSync",
-  tag = 'v0.9.1',
+  tag = 'v0.9.3',
   config = function()
     local treesitter = require('nvim-treesitter.configs')
     local vim = vim
