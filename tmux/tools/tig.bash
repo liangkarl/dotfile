a806027815524a14a93d2923bf362e4750cdc368
Karl Liang <liang.karl@outlook.com>
2025-06-02 14:03:01 -0600

    tmux: move tools/ to tmux-cmder project

--- tmux/tools/tig.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# basic information
-session="$(tmux display-message -p '#S')"
-window="$(tmux display-message -p '#W')"
-client="$(tmux display-message -p '#{client_name}')"
-
-tig || tmux display-message -c ${client} "'tig' report error $?"
