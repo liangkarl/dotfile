5097796733d2e51db2a0f70bd0f967fc22189229
Karl Liang <liang.karl@outlook.com>
2025-08-19 09:52:02 -0600

    zsh: create zsh/

---
+++ zsh/init.zsh
@@ -0,0 +1,7 @@
+# ~/.zshrc file for zsh interactive shells.
+# see /usr/share/doc/zsh/examples/zshrc for examples
+
+setopt autocd              # change directory just by typing its name
+setopt CD_SILENT
+setopt PUSHD_SILENT
+setopt HIST_FIND_NO_DUPS
