a8036782350af240ac6f5bd659cc63df42f297c5
Karl Liang <liang.karl@outlook.com>
2025-08-27 17:19:20 -0600

    nvim: update blame line format

--- nvim/config/gitsigns.lua
+++ nvim/config/gitsigns.lua
@@ -1,5 +1,9 @@
 return { -- show diff symbols aside via git diff
   'lewis6991/gitsigns.nvim',
-  config = true,
   event = "VeryLazy",
+  config = function()
+    require('gitsigns').setup({
+      current_line_blame_formatter = '[ <abbrev_sha> | <author> | <author_time:%m/%d/%y %X> | <summary> ]'
+    })
+  end,
 }
