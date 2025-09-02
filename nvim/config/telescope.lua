a6904bbbe2a2f1cb4ac344e396d3f806d4de36c1
Karl Liang <liang.karl@outlook.com>
2025-09-02 11:02:34 -0600

    nvim: fix latest project.nvim error

--- nvim/config/telescope.lua
+++ nvim/config/telescope.lua
@@ -38,7 +38,7 @@ return {
   { -- provides superior project management
     "DrKJeff16/project.nvim",
     config = function()
-      require("project_nvim").setup {
+      require("project").setup {
         -- Manual mode doesn't automatically change your root directory, so you have
         -- the option to manually do so using `:ProjectRoot` command.
         manual_mode = false,
