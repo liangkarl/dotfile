2a1a3d059dae381c630ab328306d822f3d45c74c
Karl Liang <liang.karl@outlook.com>
2025-12-29 14:59:33 -0700

    nvim: add comment for clipboard error

--- nvim/config/yanky.lua
+++ nvim/config/yanky.lua
@@ -1,3 +1,8 @@
+-- NOTE:
+-- Please refer to the link to solve the problem if you have
+-- Clipboard error : Target STRING not available
+-- https://github.com/gbprod/yanky.nvim/issues/46
+--
 return {
   "gbprod/yanky.nvim",
   opts = {
