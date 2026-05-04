919613d2f1433d759a59be1f2f7ad41792b1c00c
Karl Liang <liang.karl@outlook.com>
2026-05-04 15:16:09 -0600

    bash: force set cursor bar every time

--- bash/init/99_post-env.bash
+++ bash/init/99_post-env.bash
@@ -2,6 +2,13 @@
 
 source ${XDG_CONFIG_HOME}/bash/prompt-completion.bash
 
+# PROMPT_COMMAND=""
+force_cursor_bar() { printf '\e[?25h\e[5 q'; }
+alias fixcursor='stty sane; force_cursor_bar; clear'
+
+# Force set cursor everytime as the application may not recover it
+PROMPT_COMMAND="force_cursor_bar${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
+
 # ---------- Bash History: best-practice setup ----------
 
 # 1) Large history
