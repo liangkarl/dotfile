c333fc497d6c6ff65d5eef62c0d84a1edd617322
Karl Liang <liang.karl@outlook.com>
2026-05-08 16:29:15 -0600

    nvim: update telescope to 0.2.1

--- nvim/config/telescope.lua
+++ nvim/config/telescope.lua
@@ -68,7 +68,7 @@ return {
   },
   {
     'nvim-telescope/telescope.nvim',
-    tag = 'v0.1.9',
+    tag = 'v0.2.1',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { 'nvim-telescope/telescope-fzf-native.nvim',     build = 'make' },
