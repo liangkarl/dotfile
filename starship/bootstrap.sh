1da1f4d8285a0d294ad7bcc4b85a0534b7a2898a
Karl Liang <liang.karl@outlook.com>
2026-08-10 11:13:37 -0600

    starship: don't use oneshot as full-init.bash haven't been loaded

--- starship/bootstrap.sh
+++ starship/bootstrap.sh
@@ -3,19 +3,13 @@
 # https://github.com/b4b4r07/enhancd
 # The STARSHIP_CONFIG variable is a base directory path. It defaults to ~/.enhancd.
 
-__starship_bootstrap() {
-    local dir
+STARSHIP_HOME=$(dirname ${BASH_SOURCE[0]})
 
-    dir=$(dirname ${BASH_SOURCE[0]})
+# init ble.sh.
+# source ${dir}/ble.sh/out/ble.sh --noattach --rcfile ${dir}/blerc
 
-    # init ble.sh.
-    # source ${dir}/ble.sh/out/ble.sh --noattach --rcfile ${dir}/blerc
+# init starship
+export STARSHIP_CONFIG=${STARSHIP_HOME}/starship.toml
+eval "$(starship init bash)"
 
-    # init starship
-    export STARSHIP_CONFIG=${dir}/starship.toml
-    eval "$(starship init bash)"
-
-    # [[ ! ${BLE_VERSION-} ]] || ble-attach
-}
-
-oneshot __starship_bootstrap
+# [[ ! ${BLE_VERSION-} ]] || ble-attach
